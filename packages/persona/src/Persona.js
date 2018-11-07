53c0b838563f1a8fd561125df84874e06a99c8c8
Konstantin Zudov <konstantin.zudov@ksfmedia.fi>
2018-11-07 14:07:02 +0200

    Specify more conservative timeouts for Persona calls

--- packages/persona/src/Persona.js
+++ packages/persona/src/Persona.js
@@ -3,6 +3,12 @@ var Persona = require('persona');
 
 Persona.ApiClient.instance.basePath = process.env.PERSONA_URL;
 
+// https://visionmedia.github.io/superagent/#timeouts
+Persona.ApiClient.instance.timeout = {
+  response: 5000, // the server only has 5 seconds to respond
+  deadline: 20000 // but up to 20 seconds of overall data transfer
+};
+
 exports.loginApi = new Persona.LoginApi(Persona.ApiClient.instance);
 exports.usersApi = new Persona.UsersApi(Persona.ApiClient.instance);
 
