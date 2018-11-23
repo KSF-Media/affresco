481f795082b856e2dee283c890270a393576dfcc
Valtteri Pajunen <valtteri.pajunen@ksfmedia.fi>
2018-11-23 15:11:16 +0200

    Add Link

--- apps/subscribe-paper/src/Router.purs
+++ apps/subscribe-paper/src/Router.purs
@@ -9,6 +9,7 @@ import React.Basic as React
 
 foreign import route_ :: forall p. Fn0 (ReactComponent (RouteProps p))
 foreign import switch_ :: Fn0 (ReactComponent SwitchProps)
+foreign import link_ :: forall p. Fn0 (ReactComponent (LinkProps p))
 
 type Match =
   { params :: Foreign
@@ -18,9 +19,13 @@ type Match =
   }
 type SwitchProps = { children :: Array JSX }
 type RouteProps props = { exact :: Boolean, path :: Nullable String, component :: ReactComponent props }
+type LinkProps props = { to :: String, children :: Array String }
 
 route :: forall props. ReactComponent (RouteProps props)
 route = runFn0 route_
 
 switch :: ReactComponent SwitchProps
 switch = runFn0 switch_
+
+link :: forall props. ReactComponent (LinkProps props)
+link = runFn0 link_
