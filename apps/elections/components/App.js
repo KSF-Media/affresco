8b2682dfd897863d8f2945b94a5fc037acb8c3cd
Joakim Ekroos <joakim.ekroos@gmail.com>
2019-03-22 09:59:33 +0200

    Integrate React table (#65)
    * Add parcel-bundler
    
    * Add react-table
    
    * Table with test data
    
    * MMake table striped
    
    * Pass data to table
    
    * Add some styling and fonts
    
    * Translate UI to swedish
    
    * Style list some more

--- apps/elections/components/App.js
+++ apps/elections/components/App.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import render from 'react-dom';
 
-const App = () => (
-  <h1>Elections</h1>
-)
+import Table from './Table.js';
 
-export default App;
+import '../less/ksf-colors.less';
+import '../less/ksf-fonts.less';
+import '../assets/less/table.less';
+
+import testData from './TestData';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: testData
+    }
+  }
+  render(){
+    const { data } = this.state;
+    return (
+      <div className="ksf-elections">
+        <Table
+          data={data}
+        />
+      </div>
+    )
+  }
+}
