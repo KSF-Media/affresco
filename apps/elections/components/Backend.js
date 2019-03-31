ed8dd57bd0acf4f7b962ac41c41f7b796fbb07bb
Konstantin Zudov <konstantin.zudov@ksfmedia.fi>
2019-03-31 12:33:26 +0300

    Add backend calls for getting areas

--- apps/elections/components/Backend.js
+++ apps/elections/components/Backend.js
@@ -6,6 +6,24 @@ export function getArea(identifier) {
   return api.areasIdentifierGet(identifier);
 }
 
+export function getElectoralDistricts() {
+  return api.areasGet({ "type": ["ELECTORAL_DISTRICT"] });
+}
+
+export function getMunicipalities(electoralDistrict) {
+  return api.areasGet({
+    "type": [ "MUNICIPALITY" ],
+    "parent": electoralDistrict ? [ electoralDistrict ] : null
+  });
+}
+
+export function getPollingDistricts(municipality) {
+  return api.areasGet({
+    "type": [ "POLLING_DISTRICT" ],
+    "parent": municipality ? [ municipality ] : null
+  });
+}
+
 function makeApiClient() {
   Election.ApiClient.instance.basePath = process.env.ELECTION_BACKEND_URL;
   console.info("Loaded API client configuration", Election.ApiClient.instance)
