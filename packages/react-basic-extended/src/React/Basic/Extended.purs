e31a4f9e8dfd12488b2c074fed818a3b8e3b9c14
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2019-04-09 19:25:13 +0300

    Remove outdated compat types

--- packages/react-basic-extended/src/React/Basic/Extended.purs
+++ packages/react-basic-extended/src/React/Basic/Extended.purs
@@ -1,8 +1,5 @@
 module React.Basic.Extended
   ( module React.Basic.Compat
-  -- , ComponentSpec
-  -- , RenderArgs
-  -- , ReceivePropsArgs
   , SetState
   , SetStateThen
   , Style
@@ -13,7 +10,6 @@ import React.Basic.Compat
 
 import Data.Unit (Unit)
 import Effect (Effect)
--- import React.Basic (ReactComponentInstance)
 
 -- | In v4.0.0 this will be replaced with 'Update'.
 type SetState state = (state -> state) -> Effect Unit
@@ -23,36 +19,6 @@ type SetStateThen state =
      (state -> state)
   -> (state -> Effect Unit)
   -> Effect Unit
-
-{-
--- | In v4.0.0 this will be replaced by 'Self'
-type RenderArgs props state =
-  { props :: props
-  , state :: state
-  , setState :: SetState state
-  , setStateThen :: SetStateThen state
-  , instance_ :: ReactComponentInstance
-  }
-
-
--- | In v4.0.0 this will be replaced by 'Self'
-type ReceivePropsArgs props state =
-  { isFirstMount :: Boolean
-  , props :: props
-  , state :: state
-  , setState :: SetState state
-  , setStateThen :: SetStateThen state
-  , instance_ :: ReactComponentInstance
-  }
-
--- | Will be provided in v4.0.0
-type ComponentSpec props state =
-  { displayName :: String
-  , initialState :: state
-  , receiveProps :: ReceivePropsArgs props state -> Effect Unit
-  , render :: RenderArgs props state -> JSX
-  }
--}
 
 -- | A dummy type to give to CSS files that we `import` from foreign modules
 --   so that parcel adds them to global stylesheet.
