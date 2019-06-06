e10ee219f6fcd048f41dc0c874bdae224b3ecd68
Valtteri Pajunen <valtteri.pajunen@ksfmedia.fi>
2019-06-06 11:10:12 +0300

    Mitt konto frontpage changes (#112)
    * Add links to frequent issues
    
    * Remove phone icon and phone number
    
    * Add frequent issues link to profile page
    
    * Add margin-left to description list when xsmall screen

--- apps/mitt-konto/src/MittKonto/Main.purs
+++ apps/mitt-konto/src/MittKonto/Main.purs
@@ -172,6 +172,7 @@ userView { user } = React.fragment
         [ profileComponentBlock
         , break
         , editAccount
+        , needHelp
         , disappearingBreak
         ]
       where
@@ -234,6 +235,36 @@ userView { user } = React.fragment
             : accountEditLinks
         }
 
+    needHelp :: JSX
+    needHelp =
+      DOM.div
+        { className: "mitt-konto--need-help"
+        , children:
+            componentHeader "Behöver du hjälp?"
+            : frequentIssues
+        }
+      where
+        frequentIssues =
+          [ DOM.dl_
+              [ DOM.dt_ [ DOM.text "Frågor och svar" ]
+              , DOM.dd_ [ issueLink "HBL" "https://www.hbl.fi/fragor-och-svar/" ]
+              , DOM.dd_ [ issueLink "Västra Nyland" "https://www.vastranyland.fi/fragor-och-svar/" ]
+              , DOM.dd_ [ issueLink "Östnyland" "https://www.ostnyland.fi/ingen-tidning/" ]
+              ]
+          ,  DOM.dl_
+              [ DOM.dt_ [ DOM.text "Ingen tidning?" ]
+              , DOM.dd_ [ issueLink "HBL" "https://www.hbl.fi/ingen-tidning/" ]
+              , DOM.dd_ [ issueLink "Västra Nyland" "https://www.vastranyland.fi/ingen-tidning/" ]
+              , DOM.dd_ [ issueLink "Östnyland" "https://www.ostnyland.fi/ingen-tidning/" ]
+              ]
+          ]
+        issueLink description href =
+          DOM.a
+            { children: [ DOM.text description ]
+            , href
+            , target: "_blank"
+            }
+
     componentBlock headerText content =
       DOM.div
         { className: "mitt-konto--component-block-container"
@@ -293,7 +324,7 @@ loginView { state, setState } = React.fragment
       case state.showWelcome of
         false -> []
         true  ->
-          [ classy DOM.div "pb3 center" [ heading ]
+          [ classy DOM.div "pb2 center" [ heading ]
           , classy DOM.div "center"     [ pageDescription ]
           ]
   , classy DOM.div "center" [ loginForm ]
@@ -322,9 +353,31 @@ loginView { state, setState } = React.fragment
       classy DOM.h1 "mitt-konto--heading"
         [ DOM.text "Välkommen till KSF Media’s Mitt Konto" ]
 
+    frequentIssues =
+      classy DOM.p "mitt-konto--faq"
+        [ DOM.a
+            { href: "https://www.hbl.fi/fragor-och-svar/"
+            , children: [ DOM.text "Frågor och svar" ]
+            , target: "_blank"
+            }
+        , DOM.text " * "
+        , DOM.a
+            { href: "https://www.hbl.fi/ingen-tidning/"
+            , children: [ DOM.text "Ingen tidning" ]
+            , target: "_blank"
+            }
+        , DOM.text " * "
+        , DOM.a
+            { href: "https://www.hbl.fi/epaper/"
+            , children: [ DOM.text "Läs e-tidning" ]
+            , target: "_blank"
+            }
+        ]
+
     pageDescription =
       classy DOM.div "mitt-konto--description"
-        [ DOM.p_
+        [ frequentIssues
+        , DOM.p_
             [ DOM.text
                 """Här kan du göra tillfällig eller permanent
                    adressändring eller göra uppehåll i tidningsutdelningen.
