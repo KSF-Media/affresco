897054a3ae0515c46056c6297596a42adc1c1975
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2019-06-11 16:35:55 +0300

    Add app-article frontend (#116)
    * First import of app article view
    
    * Make parcel compatible
    
    * Remove dependencies, add xd_receiver
    
    * Remove config files
    
    * Remove REACT_APP prefix
    
    * Use Bootstrap from NPM
    
    * Add the app to a Netlify deployment
    
    * Remove build-purs script, move straight spago commands to each app package.json
    
    * Fix ruby mishap
    
    * Fix lerna

---
+++ apps/app-article/src/components/article-details.js
@@ -0,0 +1,116 @@
+import React, {Component, Fragment} from 'react';
+import PremiumBadge from "./badge";
+
+const ArticleDetails = (props) => {
+
+    let publishingDate = new Date(props.publishingTime);
+    let updatingDate = new Date(props.updateTime);
+
+    let newPublishingDate = publishingDate.getDate() + '.' + (publishingDate.getMonth() + 1) + '.' + publishingDate.getFullYear() + ' ' + (publishingDate.getHours() < 10 ? '0' : '') + publishingDate.getHours() + ':' + (publishingDate.getMinutes() < 10 ? '0' : '') + publishingDate.getMinutes();
+    let newUpdateDate = updatingDate.getDate() + '.' + (updatingDate.getMonth() + 1) + '.' + updatingDate.getFullYear() + ' ' + (updatingDate.getHours() < 10 ? '0' : '') + updatingDate.getHours() + ':' + (updatingDate.getMinutes() < 10 ? '0' : '') + updatingDate.getMinutes();
+
+    let todaysDate = new Date();
+
+    let publishedTime = (publishingDate.getHours() < 10 ? '0' : '') + publishingDate.getHours() + ':' + (publishingDate.getMinutes() < 10 ? '0' : '') + publishingDate.getMinutes();
+    let updatedTime = (updatingDate.getHours() < 10 ? '0' : '') + updatingDate.getHours() + ':' + (updatingDate.getMinutes() < 10 ? '0' : '') + updatingDate.getMinutes();
+
+    if (publishingDate.setHours(0, 0, 0, 0) === todaysDate.setHours(0, 0, 0, 0)) {
+        newPublishingDate = publishedTime;
+    }
+
+    if (updatingDate.setHours(0, 0, 0, 0) === todaysDate.setHours(0, 0, 0, 0)) {
+        newUpdateDate = updatedTime;
+    }
+
+    const isCategoryOpinion = () => {
+        return props.category === 'Opinion';
+    };
+
+    return (
+        <div className={"container"}>
+            <div className={"row articleDetails"}>
+                {
+                    isCategoryOpinion() ?
+                        <Fragment>
+                            <div className={"col-2"} style={{padding: '0px'}}>
+                                {
+                                    props.authors != null ?
+                                        props.authors.map((author, index) => {
+                                            return (
+                                                <div className={"mb-1"} key={index}>
+                                                    <img  style={{borderRadius: '50%'}} width={"38px"}
+                                                         height={"38px"}
+                                                         src={author.authorImage}
+                                                         alt=""/>
+                                                </div>
+                                            )
+                                        })
+                                        :
+                                        ''
+                                }
+                                {
+                                    props.premium ?
+                                        <PremiumBadge/>
+                                        : ''
+                                }
+                            </div>
+                            <div className={'col-5'} style={{paddingLeft: "0px"}}>
+                                {
+                                    props.authors != null ?
+                                        props.authors.map((author, index) => {
+                                            return (
+                                                <div className={"mb-2"} key={index}>
+                                                    <div className={"author"}> {author.authorByline} </div>
+                                                    <div className={"author-title"} style={{marginTop: "-3px"}}>Reporter</div>
+                                                </div>
+                                            )
+                                        })
+                                        :
+                                        ''
+                                }
+                            </div>
+                        </Fragment>
+                        :
+                        ''
+                }
+
+                {
+                    !isCategoryOpinion() ?
+                        <div className={'col-7'} style={{paddingLeft: "0px"}}>
+                            {
+                                props.authors != null ?
+                                    <div>
+                                        {
+                                            props.authors.map((author, index) => {
+                                                return (
+                                                    <div key={index}>
+                                                        <div className={"author"}> {author.authorByline} </div>
+                                                    </div>
+                                                )
+                                            })
+                                        }
+                                    </div>
+                                    : ''
+                            }
+                            {
+                                props.premium ?
+                                    <PremiumBadge/>
+                                    : ''
+                            }
+                        </div>
+                        :
+                        ''
+                }
+
+
+                <div className="col-5 pubDate text-right"
+                     style={{paddingLeft: "0px", paddingRight: "0px"}}>
+                    <div>Publicerad. {newPublishingDate}</div>
+                    <div style={{marginTop: "-3px"}}>Uppdatera. {newUpdateDate}</div>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default ArticleDetails
