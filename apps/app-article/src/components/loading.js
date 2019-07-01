0686d8270d07e38c4dbf68a30c34d30751140c58
Lulzim Fazlija <lf11661@seeu.edu.mk>
2019-07-01 16:41:11 +0300

    change loading icon

--- apps/app-article/src/components/loading.js
+++ apps/app-article/src/components/loading.js
@@ -1,17 +1,10 @@
 import React, {Component} from 'react';
+import loadingIcon from "../assets/images/loading.png";
 
-const Loading = (props) => {
+const Loading = () => {
     return(
-        <div className="sk-cube-grid">
-            <div className="sk-cube sk-cube1"></div>
-            <div className="sk-cube sk-cube2"></div>
-            <div className="sk-cube sk-cube3"></div>
-            <div className="sk-cube sk-cube4"></div>
-            <div className="sk-cube sk-cube5"></div>
-            <div className="sk-cube sk-cube6"></div>
-            <div className="sk-cube sk-cube7"></div>
-            <div className="sk-cube sk-cube8"></div>
-            <div className="sk-cube sk-cube9"></div>
+        <div className="loading">
+            <img src={loadingIcon}/>
         </div>
     )
 };
