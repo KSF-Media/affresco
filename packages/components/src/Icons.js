eede6180fedfb2cfac63d20ca7156dac93446cea
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2019-07-04 16:10:11 +0200

    Fix some more relative paths

--- packages/components/src/Icons.js
+++ packages/components/src/Icons.js
@@ -1,9 +1,9 @@
 // The path '
 exports.papers = {
-  hbl: require('../../images/logo-hbl.svg'),
-  hthl: require('../../images/logo-hthl.svg'),
-  ls: require('../../images/logo-ls.svg'),
-  on: require('../../images/logo-on.svg'),
-  vn: require('../../images/logo-vn.svg'),
-  ksf: require('../../images/logo-ksf.svg')
+  hbl: require('../../../../images/logo-hbl.svg'),
+  hthl: require('../../../../images/logo-hthl.svg'),
+  ls: require('../../../../images/logo-ls.svg'),
+  on: require('../../../../images/logo-on.svg'),
+  vn: require('../../../../images/logo-vn.svg'),
+  ksf: require('../../../../images/logo-ksf.svg')
 };
