c1222beebf7451ed4b73be241c993835b946be34
Lulzim Fazlija <lf11661@seeu.edu.mk>
2019-07-16 17:58:35 +0300

    remove share and soundicons

--- apps/app-article/src/components/footer.js
+++ apps/app-article/src/components/footer.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 const Footer = (props) => {
     return(
         <div className={"footer"}>
-            <a href="https://docs.google.com/forms/d/e/1FAIpQLSdu6q3J4b-FIQQfx2rGfPq0NUbWsc8hYYZP0unCHpt22thyAg/viewform?usp=sf_link">Ge
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLSdORLcRq_URIS7deTkgOKfGd1fy59lkdTEtEbt5bzvaADCDKw/viewform?usp=sf_link">Ge
                 respons</a>
             <br/><strong>KSF Media {new Date().getFullYear()}</strong>
         </div>
