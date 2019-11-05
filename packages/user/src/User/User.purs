05a587505c81dab08067dbffa179b3cd343e25f8
Valtteri Pajunen <valtteri.pajunen@ksfmedia.fi>
2019-11-05 13:36:20 +0200

    Add temporary name (c/o) to temporary address change (#203)
    * Add temporary name to temporary address change

--- packages/user/src/User/User.purs
+++ packages/user/src/User/User.purs
@@ -41,7 +41,8 @@ import KSF.LocalStorage as LocalStorage
 import KSF.User.Login.Facebook.Success as Facebook.Success
 import KSF.User.Login.Google as Google
 import Persona (User, MergeToken, Provider(..), UUID, Email(..), Token(..), InvalidPauseDateError(..),
-                InvalidDateInput(..), UserUpdate(..)) as PersonaReExport
+                InvalidDateInput(..), UserUpdate(..), DeliveryAddress, PendingAddressChange, Address,
+                SubscriptionState(..), Subscription, PausedSubscription, SubscriptionDates) as PersonaReExport
 import Persona as Persona
 import Record as Record
 import Unsafe.Coerce (unsafeCoerce)
@@ -348,9 +349,10 @@ temporaryAddressChange
   -> DateTime
   -> String
   -> String
+  -> Maybe String
   -> Aff (Either Persona.InvalidDateInput Persona.Subscription)
-temporaryAddressChange userUuid subsno startDate endDate streetAddress zipCode = do
-  addressChangedSub <- try $ Persona.temporaryAddressChange userUuid subsno startDate endDate streetAddress zipCode <<< _.token =<< requireToken
+temporaryAddressChange userUuid subsno startDate endDate streetAddress zipCode temporaryName = do
+  addressChangedSub <- try $ Persona.temporaryAddressChange userUuid subsno startDate endDate streetAddress zipCode temporaryName <<< _.token =<< requireToken
   case addressChangedSub of
     Right sub -> pure $ Right sub
     Left err
