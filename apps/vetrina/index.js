d4d02fd3356788b779d6f5540b4cce2cad88f3df
Konstantin Zudov <konstantin.zudov@ksfmedia.fi>
2020-01-13 15:06:38 +0200

    Initialize Vetrina app

---
+++ apps/vetrina/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// yup, welcome to react 16
+import createReactClass from 'create-react-class';
+React.createClass = createReactClass;
+var Main = require('./output/Vetrina.Main/index.js');
+
+function main() {
+  const myComponent = (
+    <Main.app/>
+  );
+
+  ReactDOM.render(myComponent, document.getElementById('app'));
+}
+
+
+if (module.hot) {
+  module.hot.accept(function () {
+    console.log('running main again');
+    main();
+  });
+}
+
+console.log('starting');
+main();
