52d6a6a03dc05f9c2a3419ffc27a44aefb4ae743
amorcian <morciano.andrea92@gmail.com>
2020-01-15 11:20:23 +0200

    Add country code field to persona call

--- packages/user/src/User/User.purs
+++ packages/user/src/User/User.purs
@@ -357,8 +357,8 @@ temporaryAddressChange
   -> String
   -> Maybe String
   -> Aff (Either Persona.InvalidDateInput Persona.Subscription)
-temporaryAddressChange userUuid subsno startDate endDate streetAddress zipCode _ temporaryName = do
-  addressChangedSub <- try $ Persona.temporaryAddressChange userUuid subsno startDate endDate streetAddress zipCode temporaryName <<< _.token =<< requireToken
+temporaryAddressChange userUuid subsno startDate endDate streetAddress zipCode countryCode temporaryName = do
+  addressChangedSub <- try $ Persona.temporaryAddressChange userUuid subsno startDate endDate streetAddress zipCode countryCode temporaryName <<< _.token =<< requireToken
   case addressChangedSub of
     Right sub -> pure $ Right sub
     Left err
