63caa88015db21a514466e244f65501788ce92cb
Valtteri Pajunen <valtteri.pajunen@ksfmedia.fi>
2020-01-30 16:32:58 +0200

    Add submit button

--- apps/vetrina/src/Vetrina/Main.purs
+++ apps/vetrina/src/Vetrina/Main.purs
@@ -2,13 +2,22 @@ module Vetrina.Main where
 
 import Prelude
 
+import Control.Alt ((<|>))
+import Data.Array (all)
+import Data.Array as Array
+import Data.Either (Either(..))
+import Data.Maybe (Maybe(..))
+import Data.Validation.Semigroup (toEither, unV)
+import Effect (Effect)
+import Effect.Class.Console as Console
+import KSF.InputField.Component as InputField
+import KSF.ValidatableForm (isNotInitialized)
+import KSF.ValidatableForm as Form
 import React.Basic (JSX)
 import React.Basic.Compat as React
 import React.Basic.DOM as DOM
-import Data.Maybe (Maybe(..))
-
-import KSF.InputField.Component as InputField
-import KSF.ValidatableForm as Form
+import React.Basic.DOM.Events (preventDefault)
+import React.Basic.Events (handler)
 
 type Props = {}
 type State =
@@ -42,8 +51,20 @@ render :: Self -> JSX
 render self =
   DOM.div
     { className: "vetrina--new-account-container"
-    , children: [ emailAddressInput self ]
+    , children: newAccountForm self
+        [ emailAddressInput self
+        , confirmButton self
+        ]
     }
+
+newAccountForm :: Self -> Array JSX -> Array JSX
+newAccountForm self children =
+  Array.singleton $
+    DOM.form
+      { className: "vetrina--new-account-form"
+      , onSubmit: handler preventDefault $ (\_ -> submitNewAccountForm self $ formValidations self)
+      , children
+      }
 
 emailAddressInput :: Self -> JSX
 emailAddressInput self@{ state: { form }} = InputField.inputField
@@ -58,3 +79,29 @@ emailAddressInput self@{ state: { form }} = InputField.inputField
   , validationError: Form.inputFieldErrorMessage $ Form.validateField EmailAddress form.emailAddress self.state.serverErrors
   , value: form.emailAddress
   }
+
+
+submitNewAccountForm :: Self -> Form.ValidatedForm NewAccountInputField NewAccountForm -> Effect Unit
+submitNewAccountForm self@{ state: { form } } = unV
+  (\errors -> self.setState _ { form { emailAddress = form.emailAddress <|> Just "" } })
+  (\validForm -> Console.log "YO!")
+
+confirmButton :: Self -> JSX
+confirmButton self =
+  DOM.input
+    { type: "submit"
+    , className: "registration--create-button mt2"
+    , disabled: isFormInvalid
+    , value: "Skapa konto"
+    }
+  where
+    isFormInvalid
+      | Left errs <- toEither $ formValidations self
+      = not $ all isNotInitialized errs
+      | otherwise = false
+
+formValidations :: Self -> Form.ValidatedForm NewAccountInputField NewAccountForm
+formValidations self@{ state: { form } } =
+  { emailAddress: _
+  }
+  <$> Form.validateField EmailAddress form.emailAddress []
