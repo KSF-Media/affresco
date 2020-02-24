aabd0cfa0829287dd17f9a8a9db9b5513c5ecb37
Valtteri Pajunen <valtteri.pajunen@ksfmedia.fi>
2020-02-24 10:45:27 +0200

    Add purchase processing view

---
+++ apps/vetrina/src/Vetrina/Purchase/Processing.purs
@@ -0,0 +1,21 @@
+module Vetrina.Purchase.Processing where
+
+import React.Basic (JSX, make)
+import React.Basic as React
+import React.Basic.DOM as DOM
+
+type Self = React.Self Props State
+type Props = {}
+type State = {}
+
+component :: React.Component Props
+component = React.createComponent "PurchaseCompleted"
+
+processing :: Props -> JSX
+processing = make component
+  { initialState: {}
+  , render
+  }
+
+render :: Self -> JSX
+render self = DOM.div_ [DOM.text "Vänligen vänta. Vi behandlar din beställning."]
