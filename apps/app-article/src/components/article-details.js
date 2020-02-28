f06dae5e8f113fd028105b5c4b3dabdecd7d6ff9
Lulzim Fazlija <lf11661@seeu.edu.mk>
2020-02-28 13:58:49 +0200

    check if field exists before accessing

--- apps/app-article/src/components/article-details.js
+++ apps/app-article/src/components/article-details.js
@@ -61,7 +61,7 @@ const ArticleDetails = (props) => {
                                 }
                                 {
                                     props.premium ?
-                                        <div className="article-info"><span className="article-opinion-type">{props.articleTypeDetails.title}</span> <PremiumBadge/></div>
+                                        <div className="article-info"><span className="article-opinion-type">{!!props.articleTypeDetails ? props.articleTypeDetails.title : ''}</span> <PremiumBadge/></div>
                                         : ''
                                 }
                             </div>
