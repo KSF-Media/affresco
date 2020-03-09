964beea29f706fb1d5346e60257893926b5f303b
Valtteri Pajunen <valtteri.pajunen@ksfmedia.fi>
2020-03-09 15:58:07 +0200

    Make Sentry play nice even when dsn not found in environment

--- packages/components/src/Sentry.js
+++ packages/components/src/Sentry.js
@@ -1,18 +1,34 @@
 exports.initSentry_ = function(sentryDsn) {
-    var Sentry = require('@sentry/browser');
-    Sentry.init({ dsn: sentryDsn });
-    window.sentry = Sentry;
-    return Sentry;
+    var Sentry = require('@sentry/browser')
+    if (sentryDsn && sentryDsn.length > 1) {
+        Sentry.init({ dsn: sentryDsn });
+        window.sentry = Sentry;
+        return Sentry;
+    }
+    else {
+        console.warn("Could not setup Sentry, dsn is faulty. Look into your env variables.");
+        return null;
+    }
 };
+
 exports.captureMessage_ = function(sentry, message, level) {
-    return sentry.captureMessage(message, level);
+    return sendSentryEvent(sentry, 'captureMessage', message, level);
 };
 exports.captureException_ = function(sentry, err) {
-    return sentry.captureException(err);
+    return sendSentryEvent(sentry, 'captureException', err);
 };
 exports.setTag_ = function(sentry, key, value) {
-    return sentry.setTag(key, value);
+    return sendSentryEvent(sentry, 'setTag', key, value);
 };
 exports.setUser_ = function(sentry, cusno) {
-    return sentry.setUser({id: cusno});
+    return sendSentryEvent(sentry, 'setUser', {id: cusno});
 };
+
+function sendSentryEvent(sentry, fnName, ...args) {
+    if (sentry && typeof sentry[fnName] === 'function') {
+        return sentry[fnName](...args);
+    }
+    else {
+      console.log('Tried to send something to Sentry, but not initialized.', fnName, args);
+    }
+}
