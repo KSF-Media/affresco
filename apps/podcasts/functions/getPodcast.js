23aee011a677c7a7d53df544f1ec38675b20ced0
Joakim Ekroos <joakim.ekroos@gmail.com>
2020-04-03 18:10:36 +0300

    Podcasts: Use rss feeds instead of api (#312)
    * Podcasts: deployment urls
    
    * Generate yarn.lock
    
    * Try without importing fonts
    
    * Add lambda-build to netlify.toml
    
    * Put back podcast ids in env vars
    
    * fix build command
    
    * Try moving main less import to App.js
    
    * bring back fonts
    
    * Move parcel to devDependencies
    
    * Tinker with parcel-bundler
    
    * Install stuff pre-build
    
    * Make podcast app more configurable
    
    * Podcast styles
    
    * Update yarn.lock
    
    * Mark all podcasts active if none selected
    
    * Add new line at end

---
+++ apps/podcasts/functions/getPodcast.js
@@ -0,0 +1,31 @@
+const axios = require('axios');
+const convert = require('xml-js');
+
+exports.handler = function(event, context, callback) {
+    const ID = event.queryStringParameters.id;
+    const URL = `https://feeds.soundcloud.com/users/soundcloud:users:${ID}/sounds.rss`;
+
+    // Send response
+    const send = body => {
+        callback(null, {
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+            },
+            statusCode: 200,
+            body: body
+        });
+    }
+
+    // Get Podcast
+    const getPodcast = () => {
+        axios.get(URL)
+        .then(resp => send(convert.xml2json(resp.data, {compact: true})))
+        .catch(err => send(err));
+    }
+
+    // Run
+    if (event.httpMethod === 'GET' && ID) {
+        getPodcast();
+    }
+}
--- apps/podcasts/src/App.js
+++ apps/podcasts/src/App.js
@@ -1,89 +1,96 @@
 import React from "react";
-import {Route, Switch, HashRouter} from "react-router-dom";
 
-import LatestTracksList from "./LatestTracsklist.js";
+import Track from "./Track.js";
+import "./less/main.less"
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      loaded: false,
-      userIds: process.env.PODCAST_IDS.split(",") || [],
-      users: [],
+      podcastIds: window.pod_ids || process.env.PODCAST_IDS.split(',') || [],
+      podAppUlr: window.pod_app_url || '',
+      podcasts: [],
       tracks: [],
-      selectedUser: null
+      selectedPodcast: null,
+      loading: null
     }
   }
 
   componentDidMount() {
-    const users = this.state.userIds.map(id => 
-      fetch(`https://api.soundcloud.com/users/${id}/?client_id=${process.env.CLIENT_ID}`)
-      .then(res => res.json())
+    this.setState({loading: true});
+    const podRequests = this.state.podcastIds.map(id => 
+      fetch(process.env.NODE_ENV === 'development'
+        ? `http://localhost:9000/getPodcast?id=${id}`
+        : `${this.state.podAppUlr}/.netlify/functions/getPodcast?id=${id}`
+      ).then(res => res.json())
     );
-    Promise.all(users).then(
-      json => {
-        this.setState({users: [...json]});
-      }
-    )
-
-    const tracks = this.state.userIds.map(id => 
-      fetch(`https://api.soundcloud.com/users/${id}/tracks/?client_id=${process.env.CLIENT_ID}`)
-      .then(res => res.json())
-    );
-    Promise.all(tracks).then(
-      json => {
-        this.setState({tracks: this.state.tracks.concat(...json)});
-      }
-    )
+    Promise.all(podRequests).then(jsons => {
+      jsons.map(json => {
+        let channel = json.rss.channel;
+        let tracks = channel.item || [];
+        let podcast = channel.title._text || null;
+        this.setState({
+          tracks: this.state.tracks.concat(tracks.map(t => {
+            return {...t, podcast};
+          })),
+          podcasts: this.state.podcasts.concat(podcast),
+          loading: false
+        });
+      });
+    });
   }
 
-  selectUser = (id) => {
+  // Select podcast
+  selectPodcast = (name) => {
     this.setState({
-      selectedUser: id
+      selectedPodcast: name
     })
   }
 
   render() {
-
-    const usersList = this.state.users.map((u, key) => {
-      let buttonClass = this.state.selectedUser === u.id ? 'active' : '';
-      return (<li className={buttonClass} onClick={() => this.selectUser(u.id)} key={u.id}>{u.username}</li>)
+    // JSX: list of podcasts
+    const podcatsList = this.state.podcasts.map((name, key) => {
+      let buttonClass = (this.state.selectedPodcast === name || !this.state.selectedPodcast) ? 'active' : '';
+      return <li className={buttonClass} onClick={() => this.selectPodcast(name)} key={key}>{name}</li>
     });
-    const resetList = this.state.selectedUser
-      ? <li onClick={() => {this.setState({selectedUser: null})}}>Se alla</li>
+    // JSX: Rest button to show all podcasts
+    const resetListButton = this.state.selectedPodcast
+      ? <li onClick={() => {this.setState({selectedPodcast: null})}}>Se alla</li>
+      : null;
+    // JSX: Podcasts nav menu
+    const podcastNav = this.state.podcasts.length > 1
+      ? <ul className="pod-nav"><li className="label">Välj podd:</li>{podcatsList}{resetListButton}</ul>
+      : null
+    // JSX: Loading indicator
+    const spinner = this.state.loading === true
+      ? <div>Laddar...</div>
       : null;
 
-    const filteredTracks = this.state.selectedUser
-      ? this.state.tracks.filter(t => t.user_id === this.state.selectedUser)
+    // Filter tracks by selected podcast
+    const filteredTracks = this.state.selectedPodcast  
+      ? this.state.tracks.filter(track => track.podcast === this.state.selectedPodcast)
       : this.state.tracks;
-
+    // Sorts tracks by pubDate
     const sortedTracks = filteredTracks.sort((a,b) => {
-      const x = new Date(a.created_at).getTime();
-      const y = new Date(b.created_at).getTime();
+      const x = new Date(a.pubDate._text).getTime();
+      const y = new Date(b.pubDate._text).getTime();
       return y - x;
-    })
+    });
+    // JSX: List of tracks
+    const trackList = sortedTracks.map((t, k) => 
+      <Track t={t} key={k} selectPodcast={this.selectPodcast} />
+    );
 
     return (
       <div className="pod-app">
-        <ul className="pod-nav">
-          <li className="label">Filtrera:</li>
-          {usersList}
-          {resetList}
-        </ul>
-        <HashRouter>
-          <Switch>
-            <Route
-              exact path="/"
-              render={() => <LatestTracksList tracks={sortedTracks} selectUser={this.selectUser} />} />
-            <Route
-              path="/:uid"
-              render={(props) => <LatestTracksList {...props} tracks={sortedTracks} selectUser={this.selectUser} />} />
-          </Switch>
-        </HashRouter>
+        {podcastNav}
+        {spinner}
+        <div className="pod-tracklist">
+          {trackList}
+        </div>
       </div>
     )
   }
-
 }
 
 export default App;
