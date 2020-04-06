cebef0b6d42d253d8c210d748d81fcbfcae5c5bb
Valtteri Pajunen <valtteri.pajunen@ksfmedia.fi>
2020-04-06 14:23:09 +0300

    This module does not have to be a react component

--- apps/vetrina/src/Vetrina/Purchase/Completed.purs
+++ apps/vetrina/src/Vetrina/Purchase/Completed.purs
@@ -2,11 +2,10 @@ module Vetrina.Purchase.Completed where
 
 import Prelude
 
-import Data.Maybe (Maybe(..), fromMaybe)
+import Data.Maybe (Maybe, fromMaybe)
 import Effect (Effect)
 import KSF.User as User
-import React.Basic (JSX, make)
-import React.Basic as React
+import React.Basic (JSX)
 import React.Basic.DOM as DOM
 import React.Basic.Events (handler_)
 import Vetrina.Types (AccountStatus(..))
@@ -17,33 +16,20 @@ type Props =
   , accountStatus :: AccountStatus
   }
 
-type State = { user :: Maybe User.User  }
-
-type Self = React.Self Props State
-
-component :: React.Component Props
-component = React.createComponent "PurchaseCompleted"
-
 completed :: Props -> JSX
-completed = make component
-  { initialState: { user: Nothing }
-  , render
-  }
-
-render :: Self -> JSX
-render self =
-  DOM.h1_ [ case self.props.accountStatus of
+completed props =
+  DOM.h1_ [ case props.accountStatus of
                ExistingAccount -> DOM.text "Tack för din beställning!"
                NewAccount      -> DOM.text "Ditt KSF Media konto är klart!"
           ]
   <> DOM.p_ [ DOM.text "Du kan nu läsa Premiumartiklar på HBL.fi" ] -- TODO: Should this come from props?
-  <> DOM.p_ [ DOM.text $ "Vi har skickat ett bekräftelses-epost till " <> (fromMaybe "" $ map _.email self.props.user) ]
-  <> completeButton self
+  <> DOM.p_ [ DOM.text $ "Vi har skickat ett bekräftelses-epost till " <> (fromMaybe "" $ map _.email props.user) ]
+  <> completeButton props
 
-completeButton :: Self -> JSX
-completeButton self =
+completeButton :: Props -> JSX
+completeButton props =
   DOM.button
     { className: "vetrina--button vetrina--completed-close"
     , children: [ DOM.text "Tillbaka till artikeln" ] -- TODO: This text may vary depending on use case
-    , onClick: handler_ $ self.props.onClose
+    , onClick: handler_ $ props.onClose
     }
