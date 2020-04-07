0501a0dc62e83cef2759ec03189d47efd66474e7
Joakim Ekroos <joakim.ekroos@gmail.com>
2020-04-07 11:35:50 +0300

    Pdcasts: make Track key unique

--- apps/podcasts/src/App.js
+++ apps/podcasts/src/App.js
@@ -77,8 +77,10 @@ class App extends React.Component {
       return y - x;
     });
     // JSX: List of tracks
-    const trackList = sortedTracks.map((t, k) => 
-      <Track t={t} key={k} selectPodcast={this.selectPodcast} />
+    const trackList = sortedTracks.map((t, k) => {
+      let pid = t.guid._text.split('/')[1];
+      return <Track t={t} key={`${pid}-${k}`} selectPodcast={this.selectPodcast} expanded={false} />
+    }
     );
 
     return (
