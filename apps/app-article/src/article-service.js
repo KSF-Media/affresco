c7cbded86fa72499bc9a6a5d77e0bee8e6976331
Lulzim <lulzim.fazlija@digia.com>
2020-05-05 14:45:35 +0300

    remove empty string

--- apps/app-article/src/article-service.js
+++ apps/app-article/src/article-service.js
@@ -11,13 +11,13 @@ const articleApi = {
 
     },
     getLatestArticles() {
-        return fetch(config.apiUrl + "latest?start=0&limit=10&paper=" + getBrandValueParam() + "", {
+        return fetch(config.apiUrl + "latest?start=0&limit=10&paper=" + getBrandValueParam(), {
             method: 'GET',
         })
             .then(response => response.json())
     },
     getMostReadArticles() {
-        return fetch(config.apiUrl + "mostread?start=0&limit=10&paper=" + getBrandValueParam() + "", {
+        return fetch(config.apiUrl + "mostread?start=0&limit=10&paper=" + getBrandValueParam(), {
             method: 'GET',
         })
             .then(response => response.json())
