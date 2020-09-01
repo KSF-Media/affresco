2eab5a6b9160c967474ef664b3429e33bc5d10bc
amorcian <morciano.andrea92@gmail.com>
2020-09-01 15:30:31 +0200

    Add Modal component

---
+++ packages/components/src/Modal.purs
@@ -0,0 +1,28 @@
+module KSF.Modal where
+
+import React.Basic (JSX, createComponent, make)
+import React.Basic as React
+import React.Basic.DOM as DOM
+
+data Visibility = Visible | Hidden
+
+type Self = React.Self Props {}
+
+type Props =
+  { visibility :: Visibility
+  }
+
+component :: React.Component Props
+component = React.createComponent "Modal"
+
+modal :: Props -> JSX
+modal = make component
+  { initialState: {}
+  , render
+  }
+
+render :: Self -> JSX
+render { props } =
+  DOM.div
+    { className: "modal--container"
+    }
