70ce47efeed36d88e12fe307983646523aaf00b6
Andrea Morciano <morciano.andrea92@gmail.com>
2020-11-05 16:12:57 +0200

    Patch CreditCard.Menu.Item module
    * Use label tag for an item, add initial style

--- packages/components/src/CreditCard/Menu/Item.purs
+++ packages/components/src/CreditCard/Menu/Item.purs
@@ -1,5 +1,7 @@
 module KSF.CreditCard.Menu.Item where
 
+import Prelude (($))
+
 import Bottega.Models (CreditCard)
 import React.Basic as React
 import React.Basic (JSX, make)
@@ -25,19 +27,32 @@ initialState :: State
 initialState = { selected: false }
 
 render :: Self -> JSX
-render self@{ state, props: { creditCard } } = DOM.div
-                { className: wrapperClass
-                , children: [ number creditCard.maskedPan
-                            , expiryDate creditCard.expiryDate
+render self@{ state, props: { creditCard } } = DOM.label
+                { className: itemClass
+                , children: [ DOM.input $
+                                { type: "radio"
+                                , name: "credit-card-menu-item--selection"
+                                , value: creditCard.id
+                                }
+                            , DOM.div
+                                { className: "credit-card-menu-item--radio-button"
+                                , id: creditCard.id
+                                , children:
+                                    [ DOM.div { className: "vetrina--product-radio-button_checked" } ]
+                                }
+                            , DOM.div_
+                                [ number creditCard.maskedPan
+                                , expiryDate creditCard.expiryDate
+                                ]
                             ]
                 }
   where
-    wrapperClass :: String
-    wrapperClass = if state.selected 
+    itemClass :: String
+    itemClass = if state.selected 
                    then 
-                     "credit-card-menu-item--wrapper"
+                     "credit-card-menu-item"
                    else
-                     "credit-card-menu-item--wrapper selected"
+                     "credit-card-menu-item selected"
 
     number :: String -> JSX
     number n = DOM.div 
