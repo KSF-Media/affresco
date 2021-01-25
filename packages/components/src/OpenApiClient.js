32cc901342c6f3a82f8db7006e65e1fa97ead13a
Valtteri Pajunen <valtteri.pajunen@ksfmedia.fi>
2021-01-25 11:53:41 +0200

    Fix arity problems

--- packages/components/src/OpenApiClient.js
+++ packages/components/src/OpenApiClient.js
@@ -1,11 +1,18 @@
 "use strict";
 const util = require('util');
 
-exports.callApi_ = function(api, methodName, params, opts) {
+exports.callApi_ = function (api, methodName, params, opts) {
   var debugInfo = { api: api, methodName: methodName, params: params, opts: opts };
-  return function(onError, onSuccess) {
-    var args = isEmptyObject(opts) ? params : params.concat(opts);
-    var req = api[methodName].apply(api, args.concat(function(err, data, res) {
+  return function (onError, onSuccess) {
+    /*
+    We need to do this parameter juggle (with `apply`) because the generated client function arity depends if it takes any `param` or not.
+    For example, when getting an article from Lettera, the function `articleUuidGet` has the definition `function(uuid, opts, callback) { .. }`
+    However Bottega's `paymentMethodCreditCardGet` take no params and has the definition of `function(opts, callback) { ... }`
+    So `articleUuidGet` has arity of 3 but `paymentMethodCreditCardGet` has arity of 2. We need to take this into consideration.
+    In short: `opts` and `callback` are always given, but `params` is not.
+    */
+    var args = params.concat(opts);
+    var req = api[methodName].apply(api, args.concat(function (err, data, res) {
       if (err) {
         debugInfo.error = err;
         // If we have an error message we decode it and attach it as the `data`
@@ -18,23 +25,20 @@ exports.callApi_ = function(api, methodName, params, opts) {
             console.error("Failed to parse error response body", decodeErr)
           }
         }
-        const debugString = util.inspect(debugInfo, {depth: null});
+        const debugString = util.inspect(debugInfo, { depth: null });
         console.error("Superagent error", err, debugInfo, debugString.replaceAll(/OAuth [0-9a-zA-Z]+/g, '<omitted>'));
         onError(err);
       } else {
         onSuccess(data);
       }
     }));
-    return function(cancelError, onCancelerError, onCancelerSuccess) {
+    return function (cancelError, onCancelerError, onCancelerSuccess) {
       try {
         req.abort();
         onCancelerSuccess();
-      } catch(e) {
+      } catch (e) {
         onCancelerError(e);
       }
     };
-  }
-  function isEmptyObject(obj) {
-    return Object.keys(obj).length === 0 && obj.constructor === Object;
   }
 }
