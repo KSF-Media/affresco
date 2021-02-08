a724c5cc01b9563b965c14ab2420e9b58e45e998
Lulzim <lulzim.fazlija@digia.com>
2021-02-08 15:07:50 +0200

    refactor

--- apps/app-article/src/App.js
+++ apps/app-article/src/App.js
@@ -466,7 +466,6 @@ if (window.ksfDfp) {
         Cookies.set('currentUser', JSON.stringify({ firstName: user.firstName, lastName: user.lastName, email: user.email, token: localStorage.getItem('token'), uuid: localStorage.getItem('uuid') }, {expires: 365}));
         localStorage.setItem("currentUser", JSON.stringify(user));
         this.setState({user: user});
-        this.fetchArticleFromApi(getUrlParam().has('uuid')?getUrlParam().get('uuid'):"");
         // Call Android bridge 
         try {
             Android.isLoggedIn();
