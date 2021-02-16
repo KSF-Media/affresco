62e815effb314347c023388d69ad9ab97ed07145
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2021-02-16 16:16:27 +0200

    Share compiler output folder in CI

--- deploy.rb
+++ deploy.rb
@@ -31,15 +31,7 @@ end
 
 # A hash of apps with their configuration
 # We read that from the deploy info that we use to generate the CI jobs
-apps_json = ""
-begin
-  apps_json = run_command("nix-shell ci/dhall.nix --run 'dhall-to-json <<< \"./ci/apps.dhall\"'")
-rescue Exception => e
-  # FIXME: this is here because Netlify doesn't have nix-shell.
-  # This is terrible and should be removed ASAP. Really.
-  apps_json = file = File.read('./temp-apps-deprecate-me-asap.json')
-end
-
+apps_json = run_command("nix-shell ci/dhall.nix --run 'dhall-to-json <<< \"./ci/apps.dhall\"'")
 apps_list = JSON.parse(apps_json)
 apps = apps_list.map{ |x| [x["deployDir"], x] }.to_h
 
