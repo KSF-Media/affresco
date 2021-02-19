491bf08a3b91608534014f5210284603e43f2e61
Kari Pahula <kaol@iki.fi>
2021-02-19 15:28:50 +0200

    Search: Allow directly selecting user by UUID (#501)

--- packages/components/src/Api.purs
+++ packages/components/src/Api.purs
@@ -2,6 +2,9 @@ module KSF.Api where
 
 import Prelude
 
+import Data.Either (Either(..))
+import Data.Maybe (Maybe(..))
+import Data.String.Regex as Regex
 import Simple.JSON (class ReadForeign, class WriteForeign)
 
 newtype UUID = UUID String
@@ -12,6 +15,13 @@ derive newtype instance writeforeignUUID :: WriteForeign UUID
 
 nullUuid :: UUID
 nullUuid = UUID "00000000-0000-0000-0000-000000000000"
+
+toUUID :: String -> Maybe UUID
+toUUID uuid = if test regex uuid then Just $ UUID uuid else Nothing
+  where
+    test (Left _) = const false
+    test (Right r) = Regex.test r
+    regex = Regex.regex "^[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}$" mempty
 
 newtype Token = Token String
 derive newtype instance showToken :: Show Token
