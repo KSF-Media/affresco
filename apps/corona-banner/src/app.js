c24cf954972abb9bd7210524743bb0bfc8708820
MichaelaKarkia <michaela.karkia@ksfmedia.fi>
2021-03-05 16:11:37 +0200

    mobile draft

--- apps/corona-banner/src/app.js
+++ apps/corona-banner/src/app.js
@@ -1,33 +1,38 @@
 import React from "react";
 import CoronaSvg from "./assets/covid-virus-icon.svg";
-// import "tailwindcss/tailwind.css";
+import Chevron from "../../../images/chevron.svg";
 
 export default function App() {
   return (
     <div className="corona-container">
       <header className="container-header">
-        <h1 className="banner-title text-sm">
-          Covid-19 <br className="mobile-hidden" /> i Finland
-        </h1>{" "}
+        <h1 className="banner-title">
+          Covid-19 <br /> i Finland
+        </h1>
         <img className="virus-image" src={CoronaSvg} alt="Coronavirus cell" />
       </header>
-      <div>
+      <div className="stat">
         <div className="stat-value">590</div>
-        <div className="stat-label">Smittade Nu</div>
+        <div className="stat-label">smittade nu</div>
       </div>
-      <div>
+      <div className="stat mobile-hidden">
         <div className="stat-value">186</div>
-        <div className="stat-label">Pa Sjukhus</div>
+        <div className="stat-label">på sjukhus</div>
       </div>
-      <div>
+      <div className="stat">
         <div className="stat-value">734</div>
-        <div className="stat-label">Avliona</div>
+        <div className="stat-label">avlidna</div>
       </div>
-      <div>
+      <div className="stat">
         <div className="stat-value">
           288476 <span className="stat-percent">(5,2%)</span>
         </div>
-        <div className="stat-label">Vaconerade</div>
+        <div className="stat-label">vaccinerade</div>
+      </div>
+      <div className="chevron-container">
+        <a>
+          <img className="chevron-right" src={Chevron} alt="" />
+        </a>
       </div>
     </div>
   );
