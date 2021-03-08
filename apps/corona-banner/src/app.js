284b6f04b66e53f33dc8faf7dbbca71d5621c2a6
Daniel Clarke <8128944+danielrlc@users.noreply.github.com>
2021-03-08 13:29:12 +0200

    Corona banner (#545)
    * setup corona banner
    
    * yarn lock file

---
+++ apps/corona-banner/src/app.js
@@ -0,0 +1,34 @@
+import React from "react";
+import CoronaSvg from "./assets/covid-virus-icon.svg";
+// import "tailwindcss/tailwind.css";
+
+export default function App() {
+  return (
+    <div className="corona-container">
+      <header className="container-header">
+        <h1 className="banner-title text-sm">
+          Covid-19 <br /> i Finland
+        </h1>{" "}
+        <img src={CoronaSvg} alt="Coronavirus cell" />
+      </header>
+      <div>
+        <div className="stat-value">590</div>
+        <div className="stat-label">Smittade Nu</div>
+      </div>
+      <div>
+        <div className="stat-value">186</div>
+        <div className="stat-label">Pa Sjukhus</div>
+      </div>
+      <div>
+        <div className="stat-value">734</div>
+        <div className="stat-label">Avliona</div>
+      </div>
+      <div>
+        <div className="stat-value">
+          288476 <span className="stat-percent">(5,2%)</span>
+        </div>
+        <div className="stat-label">Vaconerade</div>
+      </div>
+    </div>
+  );
+}
---
+++ apps/corona-banner/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app.js";
+import "./style.css";
+
+ReactDOM.render(<App />, document.querySelector("#corona"));
