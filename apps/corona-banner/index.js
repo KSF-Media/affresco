4387141a17683b05f26cedc9340d318abdaa4891
Valtteri Pajunen <valtteri.pajunen@ksfmedia.fi>
2021-03-09 13:21:06 +0200

    Move index one dir up

--- apps/corona-banner/index.js
+++ apps/corona-banner/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./app.js";
-import "./style.css";
+import App from "./src/app.js";
+import "./src/style.css";
 
 ReactDOM.render(<App />, document.querySelector("#corona"));
