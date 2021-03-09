012062c5cc885dbf79a44f248319d7c7840da6dc
Valtteri Pajunen <valtteri.pajunen@ksfmedia.fi>
2021-03-09 10:19:11 +0000

    Update dhall.nix

--- ci/dhall.nix
+++ ci/dhall.nix
@@ -6,7 +6,7 @@ let
       owner = "justinwoo";
       repo = "easy-dhall-nix";
       rev = "90957969850a44481c6e150350c56e8b53b29e1e";
-      sha256 = "1hsmp3cb0k554kh0jlfzpdzx2b8ndyh2gdykmw9hw41haaw16mmi=";
+      sha256 = "1hsmp3cb0k554kh0jlfzpdzx2b8ndyh2gdykmw9hw41haaw16mmi";
     }
   ) {
     inherit pkgs;
