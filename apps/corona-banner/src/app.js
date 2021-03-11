51483650faeb369a472f133af46282d57b597a40
MicciKarkia <michaela.karkia@ksfmedia.fi>
2021-03-11 12:34:21 +0200

    test api request from browser

--- apps/corona-banner/src/app.js
+++ apps/corona-banner/src/app.js
@@ -5,17 +5,19 @@ import Chevron from '../../../images/chevron.svg'
 
 export default function App() {
   const [newCases, setNewCases] = useState(590)
-  const [hospitalized, setHospitalized] = useState(186)
-  const [deaths, setDeaths] = useState(734)
+  const [hospitalised, setHospitalised] = useState(null)
+  const [deaths, setDeaths] = useState(null)
   const [vaccinated, setVaccinated] = useState(288476)
   const [vaccinatedPercentage, setVaccinatedPercentage] = useState(5.2)
 
 
   useEffect(() => {
     console.log('effect')
-    axios.get('https://cat-fact.herokuapp.com/facts').then(response => {
+    axios.get('https://w3qa5ydb4l.execute-api.eu-west-1.amazonaws.com/prod/finnishCoronaHospitalData').then(response => {
       console.log('promise fulfilled')
-      console.log(response)
+      console.log(response.data.hospitalised[1330])
+      setHospitalised(response.data.hospitalised[1330].totalHospitalised)
+      setDeaths(response.data.hospitalised[1330].dead)
     })
   }, [])
 
@@ -33,7 +35,7 @@ export default function App() {
           <div className='stat-label'>nya fall</div>
         </div>
         <div className='stat mobile-hidden'>
-          <div className='stat-value'>{hospitalized}</div>
+          <div className='stat-value'>{hospitalised}</div>
           <div className='stat-label'>på sjukhus</div>
         </div>
         <div className='stat'>
