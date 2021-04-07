b821d47f8e70eeee86341f0c9ea759ea841c437c
Valtteri Pajunen <valtteri.pajunen@ksfmedia.fi>
2021-04-07 15:08:08 +0300

    Add mosaico SPA

---
+++ apps/mosaico/web/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from "react-router-dom";
+
+// yup, welcome to react 16
+import createReactClass from 'create-react-class';
+React.createClass = createReactClass;
+var Mosaico = require('../output/Mosaico/index.js');
+
+function main() {
+    const myComponent = (
+        <Mosaico.jsApp />
+    );
+
+    ReactDOM.render(myComponent, document.getElementById('app'));
+}
+
+
+if (module.hot) {
+    module.hot.accept(function () {
+        console.log('running main again');
+        main();
+    });
+}
+
+console.log('starting');
+main();
