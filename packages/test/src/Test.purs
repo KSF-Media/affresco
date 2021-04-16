0fdf24f3ae952a9e9598d4331778afd8a811a021
Kari Pahula <kaol@iki.fi>
2021-04-16 15:54:15 +0300

    Unit test for credit card change

--- packages/test/src/Test.purs
+++ packages/test/src/Test.purs
@@ -3,9 +3,12 @@ module KSF.Test where
 import Prelude
 
 import Data.Date (Date)
-import Data.DateTime (DateTime(..))
+import Data.DateTime (DateTime(..), adjust)
 import Data.Formatter.DateTime as Format
+import Data.Int (toNumber)
+import Data.Time.Duration (Days(..))
 import Data.List as List
+import Data.Maybe (maybe)
 import Effect (Effect)
 import Effect.Aff (Aff)
 import Effect.Now as Now
@@ -35,12 +38,27 @@ formatDateSolid date = Format.format formatter $ DateTime date Helpers.midnight
       , Format.YearFull
       ]
 
-typeCreditCard :: forall a. Chrome.HasFrame a => a -> Aff Unit
-typeCreditCard page = do
-  let creditCardNumber = "4925000000000004"
+type TestCreditCard =
+  { number :: String
+  , year :: String
+  }
+
+getTestCard :: Int -> Effect TestCreditCard
+getTestCard n = do
+  now <- Now.nowDateTime
+  let getYear x =
+        let diff = Days $ 365.0 * toNumber x
+        in maybe "30"
+           (Format.format (List.fromFoldable [ Format.YearTwoDigits ])) $ adjust diff now
+  pure $ case n of
+    1 -> { number: "4539612884950332", year: getYear 10 }
+    _ -> { number: "4925000000000004", year: getYear 8 }
+
+typeCreditCard :: forall a. Chrome.HasFrame a => a -> TestCreditCard -> Aff Unit
+typeCreditCard page card = do
   let creditCardField = Chrome.Selector "#cardNumber"
   Chrome.waitFor_ creditCardField page
-  Chrome.type_ creditCardField creditCardNumber page
-  Chrome.select (Chrome.Selector "#year") "30" page -- Year 2030
+  Chrome.type_ creditCardField card.number page
+  Chrome.select (Chrome.Selector "#year") card.year page
   Chrome.type_ (Chrome.Selector "#securityCode") "666" page
   Chrome.click (Chrome.Selector "#okButton") page
