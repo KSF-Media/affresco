92f05b780c1e48fe703dbd4159d79bdc109633ab
Valtteri Pajunen <valtteri.pajunen@ksfmedia.fi>
2021-05-25 15:29:09 +0300

    Restore original src/

--- apps/app-article/src/components/article-additional.js
+++ apps/app-article/src/components/article-additional.js
@@ -1,40 +1,31 @@
-import React, { Component } from "react";
+import React, {Component} from 'react';
 import shareIcon from "../assets/images/share.png";
 import SvgIcon from "../svgIcon";
 import soundIcon from "../assets/images/sound.png";
+import { isDarkModeOn } from '../helper';
+
 
 class Additional extends Component {
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    }
 
-  componentDidMount() {}
-  onShare() {
-    Android.showIntent(process.env.SHARE_URL + this.state.uuid);
-  }
+    componentDidMount() {
 
-  render() {
-    return (
-      <div className={"row"}>
-	<div className="col-12">
-	  <p
-	    className={`preamble mt-3 ${
-	      this.props.darkModeEnabled ? "darkMode" : ""
-	    }`}
-	    style={this.customStyle()}
-	  >
-	    {this.props.preamble}
-	  </p>
-	</div>
-      </div>
-    );
-  }
+    }
+    onShare() {
+        Android.showIntent(process.env.SHARE_URL + this.state.uuid);
+    }
 
-  customStyle() {
-    return this.props.fontSize
-      ? { fontSize: this.props.fontSize + 0.05 + "rem", lineHeight: "120%" }
-      : {};
-  }
+    render() {
+        return(
+            <div className={"row"}>
+                <div className="col-12">
+                    <p className={`preamble mt-3 ${isDarkModeOn() ? 'darkMode': ''}`}>{this.props.preamble}</p>
+                </div>
+            </div>
+        )
+    }
 }
 
-export default Additional;
+export default Additional
--- apps/app-article/src/components/moble-article-list.js
+++ apps/app-article/src/components/moble-article-list.js
@@ -1,113 +1,82 @@
-import React, { Component } from "react";
+import React, {Component} from 'react';
 import hblDefaultImage from "../assets/images/hbl-fallback-img.png";
-const _ = require("lodash");
+import {getBrandValueParam, getIsLoggedFromUrl, getMode, getTokenFromUrl, getUserUuidFromUrl, isDarkModeOn} from "../helper";
+
 
 const isArray = (value) => {
-  return value && typeof value === "object" && value.constructor === Array;
+    return value && typeof value === 'object' && value.constructor === Array;
 };
 
 const getTag = (tags) => {
-  let tag = "";
-  if (tags.length > 0) {
-    tag = tags[0];
-  }
-  return tag;
+    let tag = '';
+    if (tags.length > 0) {
+        tag = tags[0];
+    }
+    return tag;
 };
 
 const formatTime = (date) => {
-  let publishingDate = new Date(date);
-  let newPublishingDate =
-    publishingDate.getDate() +
-    "." +
-    (publishingDate.getMonth() + 1) +
-    "." +
-    publishingDate.getFullYear() +
-    " " +
-    (publishingDate.getHours() < 10 ? "0" : "") +
-    publishingDate.getHours() +
-    ":" +
-    (publishingDate.getMinutes() < 10 ? "0" : "") +
-    publishingDate.getMinutes();
-  let todaysDate = new Date();
-  let publishedTime =
-    (publishingDate.getHours() < 10 ? "0" : "") +
-    publishingDate.getHours() +
-    ":" +
-    (publishingDate.getMinutes() < 10 ? "0" : "") +
-    publishingDate.getMinutes();
+    let publishingDate = new Date(date);
+    let newPublishingDate = publishingDate.getDate() + '.' + (publishingDate.getMonth() + 1) + '.' + publishingDate.getFullYear() + ' ' + (publishingDate.getHours() < 10 ? '0' : '') + publishingDate.getHours() + ':' + (publishingDate.getMinutes() < 10 ? '0' : '') + publishingDate.getMinutes();
+    let todaysDate = new Date();
+    let publishedTime = (publishingDate.getHours() < 10 ? '0' : '') + publishingDate.getHours() + ':' + (publishingDate.getMinutes() < 10 ? '0' : '') + publishingDate.getMinutes();
 
-  if (publishingDate.setHours(0, 0, 0, 0) === todaysDate.setHours(0, 0, 0, 0)) {
-    newPublishingDate = publishedTime;
-  }
-  return newPublishingDate;
+    if (publishingDate.setHours(0, 0, 0, 0) === todaysDate.setHours(0, 0, 0, 0)) {
+        newPublishingDate = publishedTime;
+    }
+    return newPublishingDate;
 };
 
 const MobileList = (props) => {
-  let relatedArticles = [];
-  if (isArray(props.articles)) {
-    relatedArticles = props.articles.map((item, index) => {
-      return (
-	<React.Fragment key={index}>
-	  <div className={"articleItem mobileListItems"}>
-	    <div className={"row"}>
-	      <a href={"/article/" + item.uuid + props.queryString}>
-		<div className={"col-8"}>
-		  <div>
-		    <span
-		      className={
-			"relatedArticlesItem" + props.darkModeEnabled
-			  ? "darkMode"
-			  : ""
-		      }
-		    >
-		      {item.title.length > 80
-			? item.title.substring(0, 80) + "..."
-			: item.title}
-		    </span>
-		  </div>
-		  <div className={"articleItemDetails"}>
-		    <div className={`category brandColor-${props.brand}`}>
-		      {getTag(item.tags)}
-		    </div>
-		    <div className={"date"}>
-		      {formatTime(item.publishingTime)}
-		    </div>
-		  </div>
-		</div>
-	      </a>
-	      <a href={"/article/" + item.uuid + ""}>
-		<div
-		  className={"col-4 articleImage"}
-		  onClick={() => {
-		    window.location.href = "?uuid=" + item.uuid + urlParams;
-		  }}
-		>
-		  {item.listImage === null ? (
-		    <img
-		      className="card-img-top"
-		      src={hblDefaultImage}
-		      alt=""
-		    />
-		  ) : (
-		    <img
-		      className="card-img-top"
-		      src={
-			item.listImage.url +
-			"&function=hardcrop&width=798&height=649&q=95"
-		      }
-		      alt=""
-		    />
-		  )}
-		</div>
-	      </a>
-	    </div>
-	  </div>
-	</React.Fragment>
-      );
-    });
+    const urlParams = "&paper=" + getBrandValueParam() + "&mode=" + getMode() + "&token=" + getTokenFromUrl() + "&userId=" + getUserUuidFromUrl() + "&isLogged=" + getIsLoggedFromUrl();
+    let relatedArticles = [];
+    if (isArray(props.articles)) {
+        relatedArticles = props.articles.map((item, index) => {
+            return (
+                <React.Fragment key={index}>
+                    <div className={"articleItem mobileListItems"}>
+                        <div className={"row"}>
+                            <div className={"col-8"} onClick={() => {
+                                window.location.href = "?uuid=" + item.uuid + urlParams;
+                            }}>
+                                <div>
+                                    <a className={`relatedArticlesItem ${isDarkModeOn() ? 'darkMode': ''}`}
+                                       href={"?uuid=" + item.uuid + urlParams}>
+                                        {
+                                            item.title.length > 80 ?
+                                                item.title.substring(0, 80) + "..."
+                                                :
+                                                item.title
+                                        }
+                                    </a>
+                                </div>
+                                <div className={"articleItemDetails"}>
+                                    <div className={`category brandColor-${getBrandValueParam()}`}>{getTag(item.tags)}</div>
+                                    <div className={"date"}>{formatTime(item.publishingTime)}</div>
+                                </div>
+                            </div>
+                            <div className={"col-4 articleImage"} onClick={() => {
+                                window.location.href = "?uuid=" + item.uuid + urlParams;
+                            }}>
+                                {
+                                    item.listImage === null ?
+                                        <img className="card-img-top"
+                                             src={hblDefaultImage}
+                                             alt=""/>
+                                        :
+                                        <img className="card-img-top"
+                                             src={item.listImage.url + "&function=hardcrop&width=798&height=649&q=95"}
+                                             alt=""/>
+                                }
+                            </div>
+                        </div>
+                    </div>
+                </React.Fragment>
+            )
+        });
 
-    return relatedArticles;
-  }
+        return relatedArticles;
+    }
 };
 
-export default MobileList;
+export default MobileList
