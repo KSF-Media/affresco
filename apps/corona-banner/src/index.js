fb7208299133a4d7c76301efd78b2cef73970de9
MicciKarkia <60072001+MicciKarkia@users.noreply.github.com>
2021-05-27 16:09:23 +0300

    Feature/add corona banner to app article (#629)
    * move banner to packages/components
    
    * move banner svgs, set up banner stats in app-article
    
    * add linkwrapper to conditionally render links in banner, move stylesheet import to banner, add banner to app-article with coronaviruset tagg
    
    * refine checking if article has correct tag to fetch banner stats
    
    * hide stat for screens up to 350px
    
    * remove console.logs
    
    * move css rules from component to app
    
    * add function argument to fetch corona banner stats if article content visible
    
    * format code from single to double quotes, add conditional rendering of percentage stat in banner
    
    * remove corona-virus-icon from corona-banner app
    
    * fix vaccinatedAmount in response data
    
    * change vaccinated stats displayed if amount or percentage is missing
    
    * remove css reset rules from banner
    
    * format code
    
    * refine function to show vaccination stats if availabe
    
    * show vaccinated based on available stats
    
    * change conditional rendering for stats to check for falsy values
    
    Co-authored-by: Valtteri Pajunen <valtteri.pajunen@ksfmedia.fi>

--- apps/corona-banner/src/index.js
+++ apps/corona-banner/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app.js";
-import "../../../less/corona-banner.less";
+import './index.css';
 
 ReactDOM.render(<App />, document.querySelector("#corona"));
