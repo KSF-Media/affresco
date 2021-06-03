d8bd4f24bbb362b9821932f6ec54721792493f66
Joakim Ekroos <joakim.ekroos@ksfmedia.fi>
2021-06-03 16:31:04 +0300

    Add some icons

---
+++ apps/mosaico/src/Article/Article.js
@@ -0,0 +1,6 @@
+exports.someIcons = {
+  facebook: require("../../../../images/icon-facebook.svg"),
+  twitter:  require("../../../../images/icon-twitter.svg"),
+  linkedin: require("../../../../images/icon-linkedin.svg"),
+  mail:     require("../../../../images/icon-mail.svg")
+};
