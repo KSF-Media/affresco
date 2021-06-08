79d334b0a4fedd09bce84436ccdb5c7a61c15c41
Joakim Ekroos <joakim.ekroos@gmail.com>
2021-06-08 10:05:21 +0300

    REmove redundant import

--- apps/mosaico/src/Ads/Ad.purs
+++ apps/mosaico/src/Ads/Ad.purs
@@ -6,7 +6,6 @@ import Data.Array (length, insertAt, snoc)
 import Data.Maybe (fromMaybe)
 import Effect (Effect)
 import Effect.Uncurried (EffectFn1, runEffectFn1)
-import Effect.Class.Console as Console
 import Payload.Internal.Utils (toLowerCase)
 import React.Basic (JSX)
 import React.Basic.Classic (make)
