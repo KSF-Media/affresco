c4852bd6274499fd7c7d69c637fe8ea0de4b2e26
Joakim Ekroos <joakim.ekroos@gmail.com>
2021-06-09 17:09:23 +0300

    Mosaico/some (#670)
    * Add some icons
    
    * Add some icons
    
    * Add sharing buttons
    
    * Add whatsapp
    
    * Clean up class names
    
    * Use nesting there
    
    Co-authored-by: Joakim Ekroos <joakim.ekroos@ksfmedia.fi>

---
+++ apps/mosaico/src/Article/Article.js
@@ -0,0 +1,7 @@
+exports.someIcons = {
+  facebook: require("../../../../images/icon-facebook.svg"),
+  twitter:  require("../../../../images/icon-twitter.svg"),
+  linkedin: require("../../../../images/icon-linkedin.svg"),
+  whatsapp: require("../../../../images/icon-whatsapp.svg"),
+  mail:     require("../../../../images/icon-mail.svg")
+};
