567e44edfb5bbc468745b5e34d4994d661f4a7fa
Joakim Ekroos <joakim.ekroos@ksfmedia.fi>
2021-06-11 13:51:30 +0300

    Remove commented-out line

--- apps/mosaico/src/Ads/Ad.js
+++ apps/mosaico/src/Ads/Ad.js
@@ -1,5 +1,4 @@
 exports.fetchAd = function (contentUnit) {
-  // console.log("Fetching ad ", contentUnit);
   window.googletag.cmd.push(function () {
     window.googletag.display(contentUnit);
   });
