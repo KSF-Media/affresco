2e181d3644b945efd333341f3e945623a4b7ab54
ae-mo <morciano.andrea92@gmail.com>
2021-06-11 15:35:16 +0300

    Refactor Mosaico.Header.Menu
    * Eliminate duplication in sections generation

--- apps/mosaico/src/Mosaico/Header/Menu.purs
+++ apps/mosaico/src/Mosaico/Header/Menu.purs
@@ -2,9 +2,9 @@ module Mosaico.Header.Menu where
 
 import Prelude
 
-import Data.Array (concat, foldl)
-import Data.List (List, (:), concat, foldl)
-import Data.List (List, (:), concat, foldl) as List
+import Data.Array ((..), concat, foldl, range)
+import Data.List (List, (:))
+import Data.List.Lazy as List
 import Data.String.Common (trim)
 import Data.Tuple (uncurry)
 import Data.Tuple.Nested ((/\))
@@ -40,52 +40,51 @@ render self@{ props: { visible } } = DOM.div
                   }
               , DOM.div
                   { className: menuContentClass <> " grid-row-2 grid-colspan-3"
-                  , children: topSections <>
-                              middleSections <>
-                              bottomSections
+                  , children: sections
                   }
               ]
   }
   where
-    topSections = uncurry defaultSection <$> [ ["grid-row-1", "grid-col-2"] /\ "E-TIDNINGEN"
-                                             , ["grid-row-1", "grid-col-3"] /\ "KUNDERSVICE"
-                                             , ["grid-row-1", "grid-col-4"] /\ "ANNONSERA"
-                                             , ["grid-row-1", "grid-col-5"] /\ "ANNAT VIKTIGT"
-                                             ]
+    sections :: Array JSX
+    sections = completeSections
+      where
+        -- produces the final list of sections
+        completeSections = concat <<< List.toUnfoldable <<< (<$>) List.toUnfoldable <<< List.zipWith (List.zipWith ($)) fixedRowsAndColumns $ List.fromFoldable [topSectionTitles, middleSectionTitles, bottomSectionTitles ]
+        -- produces a list of lists, each list contains one row of section generating functions, eeach with row and column class arguments assigned: 
+        -- [[defaultSection "grid-row-1" "grid-column-2", defaultSection "grid-row-1" "grid-column-3", ... ], [defaultSection "grid-row-2" "grid-column-2", ... ], ... ]
+        fixedRowsAndColumns = List.zipWith (<$>) fixedRows $ List.repeat gridColumns
+        -- Produces a list of section generating functions that have the row class argument assigned: [ defaultSection "grid-row-1", .. ]
+        fixedRows = List.zipWith ($) (List.fromFoldable [ defaultSection, defaultSection, greySection ]) gridRows
 
-    middleSections = uncurry defaultSection <$> [ ["grid-row-3", "grid-col-2"] /\ "STARTSIDAN"
-                                                , ["grid-row-3", "grid-col-3"] /\ "OPINION"
-                                                , ["grid-row-3", "grid-col-4"] /\ "KULTUR"
-                                                , ["grid-row-3", "grid-col-5"] /\ "SPORT"
-                                                , ["grid-row-3", "grid-col-6"] /\ "ANNAT"
-                                                ]
+        -- CSS classes for grid rows positions
+        gridRows = List.fromFoldable $ ((<>) "grid-row-" <<< show) <$> [ 1, 3, 5 ]
+        -- CSS classes for grid columns positions
+        gridColumns = List.fromFoldable $ ((<>) "grid-col-" <<< show) <$> range 2 6
 
-    bottomSections = uncurry greySection <$> [ ["grid-row-5", "grid-col-2"] /\ "KONTAKT"
-                                             , ["grid-row-5", "grid-col-3"] /\ "ANNONSERA"
-                                             , ["grid-row-5", "grid-col-4"] /\ "KUNDSERVICE"
-                                             , ["grid-row-5", "grid-col-5"] /\ "KUNDSERVICE"
-                                             ]
+        topSectionTitles = List.fromFoldable ["E-TIDNINGEN", "KUNDERSVICE", "ANNONSERA", "ANNAT VIKTIGT"]
+        middleSectionTitles = List.fromFoldable ["STARTSIDAN", "OPINION", "KULTUR", "SPORT", "ANNAT"]
+        bottomSectionTitles = List.fromFoldable["KONTAKT", "ANNONSERA", "KUNDSERVICE", "KUNDSERVICE"]
 
-    defaultSection :: Array String -> String -> JSX
+    defaultSection :: String -> String -> String -> JSX
     defaultSection = section mempty
 
-    greySection :: Array String -> String -> JSX
+    greySection :: String -> String -> String -> JSX
     greySection = section [ graySectionClass ]
 
-    section :: List.List String -> List.List String -> String -> JSX
-    section modifiers positionClasses name = DOM.div
-      { className: unwords [ sectionClass, unwords modifiers, unwords positionClasses ]
+    section :: Array String -> String -> String -> String -> JSX
+    section modifiers rowClass columnClass name = DOM.div
+      { className: unwords [ sectionClass, unwords modifiers, rowClass, columnClass ]
       , children: [ DOM.a_ [ DOM.text name ] ]
       }
 
-    unwords :: List.List String -> String
-    unwords = trim <<< List.foldl (\a w -> a <> " " <> w) mempty
+    unwords :: Array String -> String
+    unwords = trim <<< foldl (\a w -> a <> " " <> w) mempty
 
-    zipWith3 :: forall a b c d. (a->b->c->d) -> List.List a -> List.List b -> List.List c -> List.List d
+    {- zipWith3 :: forall a b c d. (a->b->c->d) -> List.List a -> List.List b -> List.List c -> List.List d
     zipWith3 z = go
       where
         go (a:as) (b:bs) (c:cs) = z a b c : go as bs cs
-        go _ _ _                = []
+        go _ _ _                = List.fromFoldable []-}
 
     headerBlock = "mosaico-header"
     menuElement = "__menu"
