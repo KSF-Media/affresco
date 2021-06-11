d89a8b51bfcc2de4f1bac52348e876e91482482e
amorcian <morciano.andrea92@gmail.com>
2021-06-11 19:38:31 +0300

    Add subsections and separators to menu

--- apps/mosaico/src/Mosaico/Header/Menu.purs
+++ apps/mosaico/src/Mosaico/Header/Menu.purs
@@ -2,7 +2,7 @@ module Mosaico.Header.Menu where
 
 import Prelude
 
-import Data.Array ((..), concat, foldl, range)
+import Data.Array ((..), concat, cons, foldl, range)
 import Data.List (List, (:))
 import Data.List.Lazy as List
 import Data.String.Common (trim)
@@ -48,9 +48,15 @@ render self@{ props: { visible } } = DOM.div
     sections :: Array JSX
     sections = completeSections
       where
-        -- produces the final list of sections
-        completeSections = concat <<< List.toUnfoldable <<< (<$>) List.toUnfoldable <<< List.zipWith (List.zipWith ($)) fixedRowsAndColumns $ List.fromFoldable [topSectionTitles, middleSectionTitles, bottomSectionTitles ]
-        -- produces a list of lists, each list contains one row of section generating functions, eeach with row and column class arguments assigned: 
+        -- produces the final array of sections
+        completeSections = concat <<< List.toUnfoldable $ List.toUnfoldable <$> withSeparators
+        -- append a separator to each list of sections
+        withSeparators = List.zipWith (<>) withSubsections separators
+        -- same structure as below, but the proper subsection array is now assigned to each section function
+        withSubsections = List.zipWith (List.zipWith ($)) withTitles subsections
+        -- same structure as below, but the proper title is now assigned to each section function
+        withTitles = List.zipWith (List.zipWith ($)) fixedRowsAndColumns sectionTitles
+        -- produces a list of lists, each list contains one row of section generating functions, eeach with row and column class arguments assigned:
         -- [[defaultSection "grid-row-1" "grid-column-2", defaultSection "grid-row-1" "grid-column-3", ... ], [defaultSection "grid-row-2" "grid-column-2", ... ], ... ]
         fixedRowsAndColumns = List.zipWith (<$>) fixedRows $ List.repeat gridColumns
         -- Produces a list of section generating functions that have the row class argument assigned: [ defaultSection "grid-row-1", .. ]
@@ -61,43 +67,83 @@ render self@{ props: { visible } } = DOM.div
         -- CSS classes for grid columns positions
         gridColumns = List.fromFoldable $ ((<>) "grid-col-" <<< show) <$> range 2 6
 
+        separators = List.fromFoldable $ List.singleton <<< separator <$> separatorRows
+
+        separatorRows = ((<>) "grid-row-" <<< show) <$> [ 2, 4, 6 ]
+
+        sectionTitles = List.fromFoldable [ topSectionTitles, middleSectionTitles, bottomSectionTitles ]
+
         topSectionTitles = List.fromFoldable ["E-TIDNINGEN", "KUNDERSVICE", "ANNONSERA", "ANNAT VIKTIGT"]
         middleSectionTitles = List.fromFoldable ["STARTSIDAN", "OPINION", "KULTUR", "SPORT", "ANNAT"]
-        bottomSectionTitles = List.fromFoldable["KONTAKT", "ANNONSERA", "KUNDSERVICE", "KUNDSERVICE"]
+        bottomSectionTitles = List.fromFoldable ["KONTAKT", "ANNONSERA", "KUNDSERVICE", "KUNDSERVICE"]
 
-    defaultSection :: String -> String -> String -> JSX
+        subsections =  List.fromFoldable [ topSubsections, middleSubsections, bottomSubsections ]
+
+        topSubsections = List.fromFoldable [[], [], [], []]
+
+        middleSubsections = List.fromFoldable [ ["Lorem", "Pellentesque", "Aliquet", "Utrices"]
+                                              , ["Lorem", "Pellentesque", "Aliquet", "Utrices"]
+                                              , ["Lorem", "Pellentesque", "Aliquet", "Utrices"]
+                                              , ["Lorem", "Pellentesque", "Aliquet", "Utrices"]
+                                              , ["Lorem", "Pellentesque", "Aliquet", "Utrices"]
+                                              ]
+
+        bottomSubsections = List.fromFoldable [ ["Lorem", "Pellentesque", "Aliquet", "Utrices"]
+                                              , ["Lorem", "Pellentesque", "Aliquet", "Utrices"]
+                                              , ["Lorem", "Pellentesque", "Aliquet", "Utrices"]
+                                              , ["Lorem", "Pellentesque", "Aliquet", "Utrices"]
+                                              ]
+
+
+    defaultSection :: String -> String -> String  -> Array String -> JSX
     defaultSection = section mempty
 
-    greySection :: String -> String -> String -> JSX
+    greySection :: String -> String -> String  -> Array String -> JSX
     greySection = section [ graySectionClass ]
 
-    section :: Array String -> String -> String -> String -> JSX
-    section modifiers rowClass columnClass name = DOM.div
+    section :: Array String -> String -> String -> String -> Array String -> JSX
+    section modifiers rowClass columnClass name subsections = DOM.div
       { className: unwords [ sectionClass, unwords modifiers, rowClass, columnClass ]
-      , children: [ DOM.a_ [ DOM.text name ] ]
+      , children: DOM.a { className: sectionTitleClass
+                        , children: [ DOM.text name ]
+                        }  `cons` (subsection <$> subsections)
       }
+
+    subsection :: String -> JSX
+    subsection name = DOM.a
+      { className: subsectionClass
+      , children: [ DOM.text name ]
+      }
+
+    separator :: String -> JSX
+    separator rowClass = DOM.hr { className: unwords [ separatorClass, rowClass, "grid-col-2", "grid-colspan-5"] }
 
     unwords :: Array String -> String
     unwords = trim <<< foldl (\a w -> a <> " " <> w) mempty
 
-    {- zipWith3 :: forall a b c d. (a->b->c->d) -> List.List a -> List.List b -> List.List c -> List.List d
-    zipWith3 z = go
-      where
-        go (a:as) (b:bs) (c:cs) = z a b c : go as bs cs
-        go _ _ _                = List.fromFoldable []-}
-
     headerBlock = "mosaico-header"
+
     menuElement = "__menu"
     visibleModifier = "--visible"
     menuClass = headerBlock <> menuElement
     menuModifierClass = menuClass <> visibleModifier
+
     searchElement = "__search"
     searchClass = headerBlock <> searchElement
+
     menuContentElement = "__menu-content"
     menuContentClass = headerBlock <> menuContentElement
+
     sectionElement = "__section"
     sectionClass = headerBlock <> sectionElement
     grayModifier = "--gray"
     graySectionClass = sectionClass <> grayModifier
+
+    sectionTitleElement = "__section-title"
+    sectionTitleClass = headerBlock <> sectionTitleElement
+
     subsectionElement = "__subsection"
     subsectionClass = headerBlock <> subsectionElement
+
+    separatorElement = "__separator"
+    separatorClass = headerBlock <> separatorElement
