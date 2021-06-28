ee028e93924cf3dceb71f9d3df68ea62b89069cd
Valtteri Pajunen <valtteri.pajunen@ksfmedia.fi>
2021-06-28 16:23:15 +0300

    Add app bridge for logging in

--- apps/app-article-server/src/browser/components/premium.js
+++ apps/app-article-server/src/browser/components/premium.js
@@ -7,19 +7,28 @@ const PremiumBox = (props) => {
       <br />
       <p>En del av vårt material är endast tillgängligt för våra prenumeranter.</p>
       <p className="cta-login-existing">
-        Redan kund?{" "}
-        <a onClick={(e) => showLogin(e)} href={"#"}>
-          Logga in
-        </a>
+	Redan kund?{" "}
+	<a onClick={(e) => showLogin(e)} href={"#"}>
+	  Logga in
+	</a>
       </p>
     </div>
   );
 };
 
 function showLogin(e) {
-  e.preventDefault();
-  // TODO: Add mobile bridge
-  console.log("SHOW LOGIN");
+  try {
+    Android.showLogin();
+  } catch (err) {
+    // we might have a crash when Android bridge is not registered
+  }
+
+  // For Ios
+  try {
+    Window.webkit.messageHandlers.showLogin.postMessage("");
+  } catch (err) {
+    // we might have a crash when Ios webkit bridge is not registered
+  }
 }
 
 export default PremiumBox;
