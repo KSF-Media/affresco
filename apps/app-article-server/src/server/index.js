78d289048a990f3910313a7d394033407d5792b5
Valtteri Pajunen <valtteri.pajunen@ksfmedia.fi>
2021-07-30 14:07:15 +0300

    Make http requests asynchronous

--- apps/app-article-server/src/server/index.js
+++ apps/app-article-server/src/server/index.js
@@ -2,6 +2,7 @@ var express = require("express");
 var React = require("react");
 var ReactDOM = require("react-dom/server");
 var app = express();
+var axios = require("axios");
 const port = 8080;
 require("dotenv").config();
 import generateHtml from "./generateHtml";
@@ -18,10 +19,10 @@ app.get("/article/:id", async (req, res) => {
   if (articleId && UUID.validate(articleId)) {
     const authHeaders = () => {
       if (req.headers.authuser && UUID.validate(req.headers.authuser) && req.headers.authorization) {
-        return {
-          authuser: req.headers.authuser,
-          authorization: req.headers.authorization,
-        };
+	return {
+	  authuser: req.headers.authuser,
+	  authorization: req.headers.authorization,
+	};
       }
     };
     const queryString = req._parsedUrl.search || "";
@@ -32,87 +33,84 @@ app.get("/article/:id", async (req, res) => {
 });
 
 async function renderArticle(articleId, res, authHeaders, queryParams, queryString) {
-  const httpGet = (url) => {
-    const requestOptions = {
-      method: "get",
-      headers: authHeaders,
-    };
-    return new Promise((resolve, reject) => {
-      https.get(url, requestOptions, (res) => {
-        res.setEncoding("utf8");
-        let body = "";
-        res.on("data", (chunk) => (body += chunk));
-        res.on("end", () => {
-          try {
-            const parsedJson = JSON.parse(body);
-            resolve(parsedJson);
-          } catch {
-            resolve({ error: body });
-          }
-        });
-      });
-    });
-  };
-
-  const articleResponse = await httpGet(process.env.LETTERA_URL + "/article/" + articleId);
-
-  // If we get some weird response or a 500, abort!
-  if (_.has(articleResponse, "error") || _.get(articleResponse, "http_code") === 500) {
-    const markup = ReactDOM.renderToString(<ErrorPage message={"Artikeln kunde inte hämtas!"} />);
-    const html = generateHtml(markup);
-    console.warn("Failed to fetch article! Error message: " + _.get(articleResponse, "error"));
-    res.send(html);
-    return;
-  }
-
-  const paper = queryParams.paper || "hbl";
-  const mostReadResponse = await httpGet(process.env.LETTERA_URL + "/mostread?paper=" + paper);
-
-  const user = _.get(authHeaders, "authuser")
-    ? await httpGet(process.env.PERSONA_URL + "/users/" + authHeaders.authuser)
-    : null;
-
-  const isPreviewArticle = articleResponse.http_code === 403 && _.has(articleResponse, "not_entitled.articlePreview");
-
-  const article = isPreviewArticle ? articleResponse.not_entitled.articlePreview : articleResponse;
-
-  const mostReadArticles = _.isArray(mostReadResponse) ? mostReadResponse : [];
-
-  const articleJSX = (
-    <Article
-      title={article.title}
-      mainImage={article.mainImage}
-      body={article.body}
-      tags={article.tags || []}
-      relatedArticles={article.relatedArticles || []}
-      preamble={article.preamble}
-      articleType={article.articleType}
-      articleTypeDetails={article.articleTypeDetails}
-      publishingTime={article.publishingTime}
-      updateTime={article.updateTime}
-      authors={article.authors}
-      premium={article.premium}
-      isPreview={isPreviewArticle}
-      mostReadArticles={mostReadArticles}
-      fontSize={queryParams.fontSize}
-      darkModeEnabled={queryParams.mode === "dark"}
-      queryString={queryString}
-      paper={paper}
-    />
-  );
-
-  const updatedArticle = _.merge(article, {
-    mostReadArticles: mostReadArticles,
-    isPreview: isPreviewArticle,
-    fontSize: queryParams.fontSize,
-    darkModeEnabled: queryParams.mode === "dark",
-    queryString: queryString,
-    paper: paper,
+  let articleReq = axios.get(process.env.LETTERA_URL + "/article/" + articleId, {
+    headers: authHeaders,
+    // Throw errors on any typical HTTP status code except 403, which is the response status of a preview article
+    validateStatus: function (httpStatus) {
+      return httpStatus < 400 || httpStatus === 403;
+    },
   });
+  const userReq = _.get(authHeaders, "authuser")
+    ? axios.get(process.env.PERSONA_URL + "/users/" + authHeaders.authuser)
+    : null;
+  const paper = queryParams.paper || "hbl";
+  const darkModeEnabled = queryParams.mode === "dark";
+  const mostReadReq = axios.get(process.env.LETTERA_URL + "/mostread?paper=" + paper);
 
-  const markup = ReactDOM.renderToString(articleJSX);
-  const html = generateHtml(markup, updatedArticle, user);
-  res.send(html);
+  const requests = [articleReq, mostReadReq, userReq].filter((r) => r !== null);
+
+  axios
+    .all(requests)
+    .then(
+      axios.spread((...responses) => {
+	const articleResponse = responses[0].data;
+
+	let article;
+	let isPreviewArticle;
+	// If the response.data is an article already, meaning we can find the article id (uuid) there, great! We have an article
+	if (_.has(articleResponse, "uuid")) {
+	  article = articleResponse;
+	  isPreviewArticle = false;
+	  // Otherwise, it might be a preview article, so we need to dig a bit deeper into the JSON object
+	} else if (_.has(articleResponse, "not_entitled")) {
+	  article = _.get(articleResponse, "not_entitled.articlePreview");
+	  isPreviewArticle = true;
+	}
+	const mostReadArticles = responses[1].data;
+	const user = _.get(responses[2], "data");
+
+	const articleJSX = (
+	  <Article
+	    title={article.title}
+	    mainImage={article.mainImage}
+	    body={article.body}
+	    tags={article.tags || []}
+	    relatedArticles={article.relatedArticles || []}
+	    preamble={article.preamble}
+	    articleType={article.articleType}
+	    articleTypeDetails={article.articleTypeDetails}
+	    publishingTime={article.publishingTime}
+	    updateTime={article.updateTime}
+	    authors={article.authors}
+	    premium={article.premium}
+	    isPreview={isPreviewArticle}
+	    mostReadArticles={mostReadArticles}
+	    fontSize={queryParams.fontSize}
+	    darkModeEnabled={queryParams.mode === "dark"}
+	    queryString={queryString}
+	    paper={paper}
+	  />
+	);
+
+	const updatedArticle = _.merge(article, {
+	  mostReadArticles: mostReadArticles,
+	  isPreview: isPreviewArticle,
+	  fontSize: queryParams.fontSize,
+	  darkModeEnabled: queryParams.mode === "dark",
+	  queryString: queryString,
+	  paper: paper,
+	});
+	const markup = ReactDOM.renderToString(articleJSX);
+	const finalHtml = generateHtml(markup, updatedArticle, user);
+	res.send(finalHtml);
+      })
+    )
+    .catch((errors) => {
+      const markup = ReactDOM.renderToString(<ErrorPage message={"Artikeln kunde inte hämtas!"} />);
+      const html = generateHtml(markup);
+      console.warn("Failed to fetch article! Error message: " + errors.data);
+      res.send(html);
+    });
 }
 
 app.listen(port, () => {
