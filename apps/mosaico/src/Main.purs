93639c8d14917bd0c6fdd9c314e9d2e96ea04f7f
Valtteri Pajunen <valtteri.pajunen@ksfmedia.fi>
2021-09-03 13:36:30 +0300

    Mosaico ssr improvement (#736)

--- apps/mosaico/src/Main.purs
+++ apps/mosaico/src/Main.purs
@@ -2,18 +2,23 @@ module Main where
 
 import Prelude
 
+import Data.Argonaut.Core (Json)
 import Data.Either (Either(..))
 import Data.List (List)
-import Data.Maybe (Maybe, fromMaybe)
+import Data.Maybe (Maybe(..), fromMaybe)
 import Data.UUID as UUID
 import Effect (Effect)
 import Effect.Aff (Aff)
 import Effect.Aff as Aff
 import Effect.Class (liftEffect)
 import Effect.Class.Console as Console
+import Effect.Uncurried (EffectFn2, EffectFn3, runEffectFn2, runEffectFn3)
 import KSF.Api (Token(..), UserAuth)
 import Lettera as Lettera
+import Mosaico.Article as Article
+import MosaicoServer as MosaicoServer
 import Node.Encoding (Encoding(..))
+import Lettera.Models (fromFullArticle, articleToJson, isPreviewArticle)
 import Node.FS.Sync as FS
 import Node.HTTP as HTTP
 import Payload.ContentType as ContentType
@@ -25,9 +30,12 @@ import Payload.Server.Handlers (File)
 import Payload.Server.Handlers as Handlers
 import Payload.Server.Response (class EncodeResponse)
 import Payload.Spec (type (:), GET, Guards, Spec(Spec), Nil)
+import React.Basic.DOM.Server as DOM
 
 -- NOTE: We need to require dotenv in JS
 foreign import requireDotenv :: Unit
+foreign import appendMosaico :: EffectFn2 String String String
+foreign import writeArticle :: EffectFn3 Json Boolean String String
 
 newtype TextHtml = TextHtml String
 instance encodeResponsePlainHtml :: EncodeResponse TextHtml where
@@ -76,17 +84,33 @@ main = do
 getArticle :: { params :: { uuid :: String }, guards :: { credentials :: Maybe UserAuth } } -> Aff TextHtml
 getArticle r@{ params: { uuid } } = do
   article <- Lettera.getArticle (fromMaybe UUID.emptyUUID $ UUID.parseUUID uuid) r.guards.credentials
-  html <- liftEffect $ FS.readTextFile UTF8 indexHtmlFileLocation
+  htmlTemplate <- liftEffect $ FS.readTextFile UTF8 indexHtmlFileLocation
+  articleComponent <- liftEffect Article.articleComponent
+  mosaico <- liftEffect MosaicoServer.app
   case article of
-    -- TODO: Add rendering for the article we get here
-    -- Now we are just returning the pre-built stuff Parcel gives us
-    Right _ -> pure $ TextHtml html
+    Right a -> do
+      let articleJSX =
+            articleComponent
+              { brand: "hbl"
+              , affArticle: pure a
+              , articleStub: Nothing
+              , onLogin: pure unit
+              , user: Nothing
+              , article: Just a
+              }
+          mosaicoString = DOM.renderToString $ mosaico { mainContent: articleJSX }
+
+      html <- liftEffect do
+        runEffectFn2 appendMosaico htmlTemplate mosaicoString
+          >>= runEffectFn3 writeArticle (articleToJson $ fromFullArticle a) (isPreviewArticle a)
+
+      pure $ TextHtml html
     Left err -> do
       Console.warn $ "Could not get article: " <> err
-      pure $ TextHtml html
+      pure $ TextHtml "Could not get article"
 
 assets :: { params :: { path :: List String } } -> Aff (Either Failure File)
-assets { params: {path} } = Handlers.directory "dist/client" path
+assets { params: { path } } = Handlers.directory "dist/client" path
 
 frontpage :: { guards :: { credentials :: Maybe UserAuth } } -> Aff TextHtml
 frontpage _ = do
