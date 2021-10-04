fe0cd85d04b4edef593c8ff1cd8cc60395e1d379
Christoffer Holmberg <mr.skug@gmail.com>
2021-10-04 11:10:15 +0300

    set keep to 10 versions as stated in mkCleanAppEngineStep

--- ci/ae-cleanup.sh
+++ ci/ae-cleanup.sh
@@ -10,7 +10,7 @@ versions=$(gcloud app versions list \
                   --service $APP_ID \
                   --sort-by '~VERSION.ID' \
                   --filter 'traffic_split = 0.0' \
-                  --format 'value(VERSION.ID)' | sed 1,5d)
+                  --format 'value(VERSION.ID)' | sed 1,10d)
 
 for version in $versions; do
     gcloud app versions delete '$version' --service $APP_ID
