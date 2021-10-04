12f542db6429466df02ebb08df592583ce36f37a
Christoffer Holmberg <mr.skug@gmail.com>
2021-10-04 10:38:36 +0300

    Fix syntax

--- ci/ae-cleanup.sh
+++ ci/ae-cleanup.sh
@@ -11,7 +11,7 @@ versions=$(gcloud app versions list \
                   --sort-by '~VERSION.ID' \
                   --filter 'traffic_split = 0.0' \
                   --format 'value(VERSION.ID)' | sed 1,5d)
+
 for version in $versions; do
-    gcloud app versions delete '$version' \
-           --service $APP_ID \
-           done
+    gcloud app versions delete '$version' --service $APP_ID
+done
