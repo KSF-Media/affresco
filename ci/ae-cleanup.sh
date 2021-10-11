788fc97200108bf93723b09510b559cef95d3957
Christoffer Holmberg <mr.skug@gmail.com>
2021-10-11 12:34:47 +0300

    No need to loop as gcloud can take a list of versions

--- ci/ae-cleanup.sh
+++ ci/ae-cleanup.sh
@@ -12,6 +12,5 @@ versions=$(gcloud app versions list \
                   --filter 'traffic_split = 0.0' \
                   --format 'value(VERSION.ID)' | sed 1,10d)
 
-for version in $versions; do
-    gcloud app versions delete '$version' --service $APP_ID
-done
+echo "Deleting $APP_ID versions: $versions"
+gcloud app versions delete $versions --service $APP_ID --quiet
