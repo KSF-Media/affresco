e5d05afc2b380ca08e59715e5924ab2f1cf03caf
Christoffer Holmberg <mr.skug@gmail.com>
2021-10-15 15:14:29 +0300

    more debug

--- deploy.rb
+++ deploy.rb
@@ -44,6 +44,7 @@ rescue Exception => e
 end
 
 puts "apps_json: '#{apps_json}'"
+puts "apps_servers_json: '#{apps_servers_json}'"
 apps_list = JSON.parse(apps_json)
 app_servers_list = JSON.parse(apps_servers_json)
 apps = apps_list.map{ |x| [x["deployDir"], x] }.to_h
