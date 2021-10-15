c3e97d8081bdae12b6a2eb50f822e20fbaa274e9
Christoffer Holmberg <mr.skug@gmail.com>
2021-10-15 15:29:54 +0300

    ...

--- deploy.rb
+++ deploy.rb
@@ -32,19 +32,17 @@ end
 
 # A hash of apps with their configuration
 # We read that from the deploy info that we use to generate the CI jobs
-apps_json = ""
-apps_servers_json = ""
-begin
-  apps_json = run_command("npx dhall-to-json <<< \"./ci/apps.dhall\"")
-  apps_servers_json = run_command("npx dhall-to-json <<< \"./ci/app-servers.dhall\"")
-rescue Exception => e
+#apps_json = ""
+#apps_servers_json = ""
+#begin
+apps_json = run_command("npx dhall-to-json <<< \"./ci/apps.dhall\"")
+apps_servers_json = run_command("npx dhall-to-json <<< \"./ci/app-servers.dhall\"")
+#rescue Exception => e
   # FIXME: this is here because Netlify doesn't have nix-shell.
   # This is terrible and should be removed ASAP. Really.
-  apps_json = file = File.read('./temp-apps-deprecate-me-asap.json')
-end
+#  apps_json = file = File.read('./temp-apps-deprecate-me-asap.json')
+#end
 
-puts "apps_json: '#{apps_json}'"
-puts "apps_servers_json: '#{apps_servers_json}'"
 apps_list = JSON.parse(apps_json)
 app_servers_list = JSON.parse(apps_servers_json)
 apps = apps_list.map{ |x| [x["deployDir"], x] }.to_h
