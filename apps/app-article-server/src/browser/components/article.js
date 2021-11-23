f629865d3e9c66be522c8a7b50ddad78b156fec5
Jaakko Kytölä <Kytola.jaakko@gmail.com>
2021-11-23 14:39:41 +0200

    Font-size tweaks, add missing badge to premium articles (#838)

--- apps/app-article-server/src/browser/components/article.js
+++ apps/app-article-server/src/browser/components/article.js
@@ -51,109 +51,116 @@ class Article extends Component {
   render() {
     return (
       <div className="article">
-	{this.state.isImageModalOpen && (
-	  <Lightbox
-	    mainSrc={this.state.modalImage + "&width=1200"}
-	    onCloseRequest={() => this.setState({ isImageModalOpen: false })}
-	    imageTitle={this.state.title}
-	    imageCaption={this.state.modalCaption}
-	    enableZoom={true}
-	  />
-	)}
+        {this.state.isImageModalOpen && (
+          <Lightbox
+            mainSrc={this.state.modalImage + "&width=1200"}
+            onCloseRequest={() => this.setState({ isImageModalOpen: false })}
+            imageTitle={this.state.title}
+            imageCaption={this.state.modalCaption}
+            enableZoom={true}
+          />
+        )}
 
-	<div className={`container-fluid article ${this.props.darkModeEnabled ? "darkMode" : ""}`}>
-	  <div>
-	    <Tag tags={this.props.tags} paper={this.props.paper} />
-	    {this.props.articleType === "Advertorial" ? (
-	      <div>
-		<div className={"row"}>
-		  <div className="advertorial-top-box">
-		    <div className="advertorial-top-box-left">ANNONS</div>
-		  </div>
-		</div>
-	      </div>
-	    ) : (
-	      ""
-	    )}
-	    <Title
-	      title={this.props.title}
-	      fontSize={this.props.fontSize}
-	      darkModeEnabled={this.props.darkModeEnabled}
-	    />
-	    <Header
-	      showHighResolutionImg={this.showHighResolutionImage}
-	      mainImage={this.props.mainImage}
-	      caption={_.get(this.props.mainImage, "caption") || ""}
-	      appendBylineLabel={
-		_.has(this.props.mainImage, "byline") &&
-		_.get(this.props.mainImage, "byline") !== null &&
-		_.get(this.props.mainImage, "byline") !== ""
-		  ? "BILD:"
-		  : ""
-	      }
-	      byline={_.get(this.props.mainImage, "byline") || ""}
-	    />
-	    <Additional
-	      preamble={this.props.preamble}
-	      increaseFontSize={this.increaseFontSize}
-	      fontSize={this.props.fontSize}
-	      darkModeEnabled={this.props.darkModeEnabled}
-	    />
-	    <ArticleDetails
-	      category={this.props.articleType}
-	      premium={this.props.premium}
-	      authors={this.props.authors}
-	      publishingTime={this.props.publishingTime}
-	      updateTime={this.props.updateTime}
-	      articleTypeDetails={this.props.articleTypeDetails}
-	      paper={this.props.paper}
-	    />
-	    <Content
-	      body={this.props.body}
-	      paper={this.props.paper}
-	      showHighResolutionImage={this.showHighResolutionImage}
-	      fontSize={this.props.fontSize}
-	      darkModeEnabled={this.props.darkModeEnabled}
-	    />
-	    <div className={"row"}>
-	      <div className={"col-sm-12"}>{this.props.isPreview ? <PremiumBox paper={this.props.paper} /> : ""}</div>
-	    </div>{" "}
-	    {this.props.relatedArticles.length > 0 ? (
-	      <RelatedArticles
-		relatedArticles={this.props.relatedArticles}
-		queryString={this.props.queryString}
-		darkModeEnabled={this.props.darkModeEnabled}
-		paper={this.props.paper}
-	      />
-	    ) : (
-	      ""
-	    )}
-	    {this.state.mostReadArticles.length > 0 ? (
-	      <MostReadArticles
-		mostReadArticles={this.state.mostReadArticles}
-		queryString={this.props.queryString}
-		darkModeEnabled={this.props.darkModeEnabled}
-		paper={this.props.paper}
-	      />
-	    ) : (
-	      ""
-	    )}
-	  </div>
-	</div>
-	<Footer brandValueName={this.props.paper} />
+        <div className={`container-fluid article ${this.props.darkModeEnabled ? "darkMode" : ""}`}>
+          <div>
+            <Tag tags={this.props.tags} paper={this.props.paper} />
+            {this.props.articleType === "Advertorial" ? (
+              <div>
+                <div className={"row"}>
+                  <div className="advertorial-top-box">
+                    <div className="advertorial-top-box-left">ANNONS</div>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              ""
+            )}
+            <Title
+              title={this.props.title}
+              fontSize={this.props.fontSize}
+              darkModeEnabled={this.props.darkModeEnabled}
+            />
+            <Header
+              showHighResolutionImg={this.showHighResolutionImage}
+              mainImage={this.props.mainImage}
+              caption={_.get(this.props.mainImage, "caption") || ""}
+              fontSize={this.props.fontSize}
+              appendBylineLabel={
+                _.has(this.props.mainImage, "byline") &&
+                _.get(this.props.mainImage, "byline") !== null &&
+                _.get(this.props.mainImage, "byline") !== ""
+                  ? "BILD:"
+                  : ""
+              }
+              byline={_.get(this.props.mainImage, "byline") || ""}
+            />
+            <Additional
+              preamble={this.props.preamble}
+              increaseFontSize={this.increaseFontSize}
+              fontSize={this.props.fontSize}
+              darkModeEnabled={this.props.darkModeEnabled}
+            />
+            <ArticleDetails
+              category={this.props.articleType}
+              premium={this.props.premium}
+              authors={this.props.authors}
+              publishingTime={this.props.publishingTime}
+              updateTime={this.props.updateTime}
+              articleTypeDetails={this.props.articleTypeDetails}
+              paper={this.props.paper}
+            />
+            <Content
+              body={this.props.body}
+              paper={this.props.paper}
+              showHighResolutionImage={this.showHighResolutionImage}
+              fontSize={this.props.fontSize}
+              darkModeEnabled={this.props.darkModeEnabled}
+            />
+            <div className={"row"}>
+              <div className={"col-sm-12"}>{this.props.isPreview ? <PremiumBox paper={this.props.paper} /> : ""}</div>
+            </div>{" "}
+            {this.props.relatedArticles.length > 0 ? (
+              <RelatedArticles
+                relatedArticles={this.props.relatedArticles}
+                queryString={this.props.queryString}
+                darkModeEnabled={this.props.darkModeEnabled}
+                paper={this.props.paper}
+              />
+            ) : (
+              ""
+            )}
+            {this.state.mostReadArticles.length > 0 ? (
+              <MostReadArticles
+                mostReadArticles={this.state.mostReadArticles}
+                queryString={this.props.queryString}
+                darkModeEnabled={this.props.darkModeEnabled}
+                paper={this.props.paper}
+              />
+            ) : (
+              ""
+            )}
+          </div>
+        </div>
+        <Footer brandValueName={this.props.paper} />
       </div>
     );
   }
 }
 
 const Title = (props, state) => {
-  const fontSizeStyle = () => (props.fontSize ? { fontSize: props.fontSize + 1 + "rem", lineHeight: "100%" } : {});
+  const classNames = new Map();
+  classNames.set("1.06", "title-xs");
+  classNames.set("1.5", "title-sm");
+  classNames.set("2.0", "title-md");
+  classNames.set("2.5", "title-lg");
+  classNames.set("3.0", "title-xl");
+
   return (
     <div className={"row"}>
       <div className={"col-12 mt-2 mb-3"} style={{ wordWrap: "break-word" }}>
-	<h2 className={`title ${props.darkModeEnabled ? "darkMode" : ""}`} style={fontSizeStyle()}>
-	  {props.title}
-	</h2>
+        <h2 className={`title ${props.darkModeEnabled ? "darkMode" : ""} ${classNames.get(props.fontSize)}`}>
+          {props.title}
+        </h2>
       </div>
     </div>
   );
