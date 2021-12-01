33f543e8c6dd1e7572a5e047abe79f4c688aa576
Kari Pahula <kaol@iki.fi>
2021-12-01 10:25:35 +0200

    Give articles and tags in lists hrefs

--- apps/mosaico/src/Mosaico/Frontpage.purs
+++ apps/mosaico/src/Mosaico/Frontpage.purs
@@ -7,16 +7,15 @@ import Data.Foldable (foldMap)
 import Data.Maybe (maybe)
 import Data.Monoid (guard)
 import Data.Newtype (un)
-import Effect (Effect)
-import Lettera.Models (ArticleStub, Tag(..))
+import Lettera.Models (ArticleStub, Tag(..), tagToURIComponent)
 import React.Basic (JSX)
 import React.Basic.DOM as DOM
-import React.Basic.Events (EventHandler, handler_)
+import React.Basic.Events (EventHandler)
 import React.Basic.Hooks (Component, component)
 
 type Props =
   { frontpageArticles :: Array ArticleStub
-  , onArticleClick :: ArticleStub -> Effect Unit
+  , onArticleClick :: ArticleStub -> EventHandler
   , onTagClick :: Tag -> EventHandler
   }
 
@@ -34,36 +33,44 @@ render props =
     renderListArticle a =
       DOM.div
         { className: "mosaico--list-article list-article-default"
+        , onClick: props.onArticleClick a
         , children:
-            [ DOM.a
-              { onClick: handler_ $ props.onArticleClick a
-              , children:
-                  [ DOM.div
-                    { className: "list-article-image"
-                    , children:[ DOM.img { src: maybe "https://cdn.ksfmedia.fi/mosaico/hbl-fallback-img.png" _.url  a.listImage } ]
-                    }
-                  , DOM.div
-                    { className: "list-article-liftup"
-                    , children:
-                        [ DOM.div
-                          { className: "mosaico-article__tag color-hbl"
-                          , onClick: foldMap props.onTagClick $ head a.tags
-                          , children: [ DOM.text $ foldMap (un Tag) (head a.tags) ]
-                          }
-                        , DOM.h2_ [ DOM.text a.title ]
-                        , guard a.premium $
-                            DOM.div
-                              { className: "mosaico--article--meta"
-                              , children:
-                                  [ DOM.div
-                                      { className: "premium-badge background-hbl"
-                                      , children: [ DOM.text "premium" ]
-                                      }
-                                  ]
-                              }
-                        ]
-                    }
-                  ]
-              }
+            [ DOM.span
+                { children:
+                    [ DOM.a
+                        { href: "/artikel/" <> a.uuid
+                        , className: "list-article-image"
+                        , children: [ DOM.img { src: maybe "https://cdn.ksfmedia.fi/mosaico/hbl-fallback-img.png" _.url  a.listImage } ]
+                        }
+                    ,  DOM.div
+                         { className: "list-article-liftup"
+                         , children:
+                             [ foldMap
+                                 (\tag ->
+                                     DOM.a
+                                       { className: "mosaico-article__tag color-hbl"
+                                       , onClick: props.onTagClick tag
+                                       , href: "/tagg/" <> tagToURIComponent tag
+                                       , children: [ DOM.text $ un Tag tag ]
+                                       }
+                                 ) $ head a.tags
+                             , DOM.a
+                                 { href: "/artikel/" <> a.uuid
+                                 , children: [ DOM.h2_ [ DOM.text a.title ] ]
+                                 }
+                             , guard a.premium $
+                               DOM.div
+                                 { className: "mosaico--article--meta"
+                                 , children:
+                                     [ DOM.div
+                                         { className: "premium-badge background-hbl"
+                                         , children: [ DOM.text "premium" ]
+                                         }
+                                     ]
+                                 }
+                             ]
+                         }
+                    ]
+                }
             ]
         }
