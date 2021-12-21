8fe0a126fbb054f3cce7fafb12001777f8839b38
Kari Pahula <kaol@iki.fi>
2021-12-21 14:54:28 +0200

    Test most read list (#886)

--- apps/mosaico/test/Test.purs
+++ apps/mosaico/test/Test.purs
@@ -11,3 +11,5 @@ type Test = Chrome.Page -> Aff Unit
 sub :: String -> Chrome.Selector -> Chrome.Selector
 sub specialize = over Chrome.Selector (_ <> specialize)
 
+site :: String
+site = "http://localhost:8080/"
