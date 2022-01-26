ac556c528aa2ff37f1c6ecda0db47ea2a2953106
Joakim Ekroos <joakim.ekroos@gmail.com>
2022-01-26 10:21:11 +0200

    Remove some console logs

--- apps/mosaico/src/Ads/Ad.js
+++ apps/mosaico/src/Ads/Ad.js
@@ -1,9 +1,7 @@
 exports.fetchAd = function (contentUnit) {
   window.googletag.cmd.push(function () {
     window.mosaicoAdSlots.map(s => {
-      // console.log(s.getSlotElementId());
       if(s.getSlotElementId() === contentUnit) {
-        console.log("Refreshing ", contentUnit);
         window.googletag.pubads().refresh([s]);
       } else {
         window.googletag.display(contentUnit);
