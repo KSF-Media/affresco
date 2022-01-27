4721bdfdd8fdce2bac92215a55409a3bf4daad1b
Joakim Ekroos <joakim.ekroos@gmail.com>
2022-01-27 10:49:29 +0200

    Mosaico/more ads (#931)
    * WIP: Admanager ads
    
    * WIP: Admanager ads
    
    * Remove some console logs
    
    * Remove the top-parade ad for now
    
    * Remove some layout-breaking ads
    
    * Remove headads from grid
    
    * Clean up comments
    
    * Use the getSlots() method instead of pushing the slots into an array
    
    Co-authored-by: Valtteri Pajunen <valtteri.pajunen@ksfmedia.fi>

--- apps/mosaico/src/Ads/Ad.js
+++ apps/mosaico/src/Ads/Ad.js
@@ -1,5 +1,11 @@
 exports.fetchAd = function (contentUnit) {
   window.googletag.cmd.push(function () {
-    window.googletag.display(contentUnit);
+    window.googletag.pubads().getSlots().map(s => {
+      if(s.getSlotElementId() === contentUnit) {
+        window.googletag.pubads().refresh([s]);
+      } else {
+        window.googletag.display(contentUnit);
+      }
+    });
   });
 };
