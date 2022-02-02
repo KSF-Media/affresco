c00be5b903834079a2647d459daa4867de005b17
MicciKarkia <michaela.karkia@ksfmedia.fi>
2022-02-02 15:29:29 +0200

    handle embedding of typeform

---
+++ apps/mosaico/static/kontakt.js
@@ -0,0 +1,4 @@
+import { createWidget } from '@typeform/embed'
+import '@typeform/embed/build/css/widget.css'
+
+createWidget('ayblJO4k', { container: document.querySelector('#kontakt'), height: 500, iframeProps: { style:"width: 100%; height: 100%; outline: none; border: none;"} })
