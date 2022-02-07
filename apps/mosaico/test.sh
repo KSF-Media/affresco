5b70eaa7b8c6e5ab4e0e75d316315c82e6966593
skug <christoffer.holmberg@ksfmedia.fi>
2022-02-07 13:40:02 +0200

    handle test failure cleanly

--- apps/mosaico/test.sh
+++ apps/mosaico/test.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
-set -e
+error_exit(){
+    echo "Tests failed, killing server."
+    kill -9 $1
+    exit 1
+}
 
 node -r dotenv/config -e 'require("./output/Main/index").main()' dotenv_config_path=.env.local &
 PIDTOKILL=$! 
 echo "Started server with pid $PIDTOKILL"
 sleep 5
 echo "Running tests"
-spago -x test.dhall test
+spago -x test.dhall test || error_exit $PIDTOKILL
 echo "Killing server $PIDTOKILL"
 kill -9 $PIDTOKILL
