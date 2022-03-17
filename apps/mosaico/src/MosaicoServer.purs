f0258f0e0d9fe1e237aa5e4bff03575cb0bb0fb2
Joakim Ekroos <joakim.ekroos@ksfmedia.fi>
2022-03-17 14:33:03 +0200

    Match dom structure so hydrating works properly

--- apps/mosaico/src/MosaicoServer.purs
+++ apps/mosaico/src/MosaicoServer.purs
@@ -7,6 +7,7 @@ import KSF.Paper as Paper
 import KSF.User (User)
 import Lettera.Models (ArticleStub, Category, Tag, categoriesMap)
 import Mosaico.Footer (footer)
+import Mosaico.Ad (ad) as Mosaico
 import Mosaico.Header as Header
 import Mosaico.Paper (mosaicoPaper)
 import Mosaico.MostReadList as MostReadList
@@ -59,7 +60,9 @@ app props =
 
 
 render :: PushStateInterface -> Props -> JSX
-render router props = DOM.div
+render router props = DOM.div_
+    [ Mosaico.ad { contentUnit: "mosaico-ad__top-parade" }
+    , DOM.div
        { className: "mosaico grid"
        , id: Paper.toString mosaicoPaper
        , children:
@@ -82,6 +85,7 @@ render router props = DOM.div
                  _ -> mempty
            ]
        }
+    ]
   where
     aside =
       DOM.aside
