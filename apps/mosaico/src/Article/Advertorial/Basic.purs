93d8606a51861040609e8734c13e4945119a7771
Valtteri Pajunen <valtteri.pajunen@ksfmedia.fi>
2022-04-04 16:40:04 +0300

    Add company name to ad banner

--- apps/mosaico/src/Article/Advertorial/Basic.purs
+++ apps/mosaico/src/Article/Advertorial/Basic.purs
@@ -2,9 +2,11 @@ module Mosaico.Article.Advertorial.Basic where
 
 import Prelude
 
-import Data.Foldable (fold)
+import Data.Foldable (fold, find, foldMap)
 import React.Basic.DOM as DOM
 import React.Basic (fragment)
+import Data.Maybe (Maybe)
+import Data.String as String
 import React.Basic.Hooks (JSX)
 
 import Lettera.Models (Article)
@@ -12,21 +14,30 @@ import Lettera.Models (Article)
 type Props = { article :: Article }
 
 render :: Props -> JSX
-render { article } = fragment [ DOM.span { className: "advertorial-top-banner", children: [ DOM.text "ayoo" ] },
-
-    DOM.article
-        { className: "mosaico-article"
-        , children:
-            [ DOM.header_
-                [ DOM.h1
-                    { className: "mosaico-article__headline"
-                    , children: [ DOM.text article.title ]
-                    }
-                , DOM.section
-                    { className: "mosaico-article__preamble"
-                    , children: [ DOM.text $ fold article.preamble ]
-                    }
-                ]
-            ]
-        }
-]
+render { article } =
+  let companyName details
+        | "companyName" <- details.title = fold details.description
+        | otherwise = mempty
+  in fragment
+       [ DOM.span
+           { className: "advertorial-top-banner"
+           , children:
+               [ DOM.span_ [ DOM.text $ "ANNONS: " <> foldMap (String.toUpper <<< companyName) article.articleTypeDetails ]
+               ]
+           }
+       , DOM.article
+           { className: "mosaico-article"
+           , children:
+               [ DOM.header_
+                   [ DOM.h1
+                       { className: "mosaico-article__headline"
+                       , children: [ DOM.text article.title ]
+                       }
+                   , DOM.section
+                       { className: "mosaico-article__preamble"
+                       , children: [ DOM.text $ fold article.preamble ]
+                       }
+                   ]
+               ]
+           }
+        ]
