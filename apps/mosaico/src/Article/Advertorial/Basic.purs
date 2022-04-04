72ad86d16daa9d966b652f84601fc42d226350a8
Valtteri Pajunen <valtteri.pajunen@ksfmedia.fi>
2022-04-04 16:17:55 +0300

    WIP: Add advertorial banner

--- apps/mosaico/src/Article/Advertorial/Basic.purs
+++ apps/mosaico/src/Article/Advertorial/Basic.purs
@@ -2,16 +2,31 @@ module Mosaico.Article.Advertorial.Basic where
 
 import Prelude
 
-import Lettera.Models (Article)
+import Data.Foldable (fold)
 import React.Basic.DOM as DOM
+import React.Basic (fragment)
 import React.Basic.Hooks (JSX)
+
+import Lettera.Models (Article)
 
 type Props = { article :: Article }
 
 render :: Props -> JSX
-render { article } =
-  DOM.article
-    { className: "mosaico-article"
-    , children:
-        [ DOM.text "ayoo" ]
+render { article } = fragment [ DOM.span { className: "advertorial-top-banner", children: [ DOM.text "ayoo" ] },
+
+    DOM.article
+        { className: "mosaico-article"
+        , children:
+            [ DOM.header_
+                [ DOM.h1
+                    { className: "mosaico-article__headline"
+                    , children: [ DOM.text article.title ]
+                    }
+                , DOM.section
+                    { className: "mosaico-article__preamble"
+                    , children: [ DOM.text $ fold article.preamble ]
+                    }
+                ]
+            ]
         }
+]
