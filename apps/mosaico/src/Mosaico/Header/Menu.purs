5aec1fbefa08dfaafdcf6117e3cd4d7851eb621b
Joakim Ekroos <joakim.ekroos@gmail.com>
2022-04-20 16:20:08 +0300

    Bring back renderSeparator

--- apps/mosaico/src/Mosaico/Header/Menu.purs
+++ apps/mosaico/src/Mosaico/Header/Menu.purs
@@ -209,8 +209,7 @@ render props@{ onLogin, onLogout } = DOM.div
           }
 
         renderSeparator :: Maybe String -> JSX
-        renderSeparator modifierClass = mempty
-        --renderSeparator modifierClass = DOM.hr { className: unwords [ separatorClass, fromMaybe mempty modifierClass ] }
+        renderSeparator modifierClass = DOM.hr { className: unwords [ separatorClass, fromMaybe mempty modifierClass ] }
 
     headerBlock = "mosaico-header"
 
