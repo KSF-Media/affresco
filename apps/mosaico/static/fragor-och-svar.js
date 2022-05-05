8f4a4089bd6208fcc4930417704c58e258b5081d
Jaakko Hannikainen <jaakko.hannikainen@bytecraft.fi>
2022-05-05 12:23:54 +0300

    Initialize static page JS only once

--- apps/mosaico/static/fragor-och-svar.js
+++ apps/mosaico/static/fragor-och-svar.js
@@ -1,54 +1,68 @@
-const questions = document.getElementsByClassName("faq__question");
+function init() {
+  window.fragorHandlersInitialized = true;
+  const questions = document.getElementsByClassName("faq__question");
 
-for (const q of questions) {
-  q.addEventListener("click", function () {
-    q.classList.toggle("faq__question--active");
+  for (const q of questions) {
+    q.addEventListener("click", function (e) {
+      e.stopPropagation();
+      e.preventDefault();
 
-    let answer = q.nextElementSibling;
-    if (answer.style.display === "block") {
-      answer.style.display = "none";
+      q.classList.toggle("faq__question--active");
+
+      let answer = q.nextElementSibling;
+      if (answer.style.display === "block") {
+        answer.style.display = "none";
+      } else {
+        answer.style.display = "block";
+      }
+    });
+  }
+
+  const showOnPx = 100;
+  const backToTopButton = document.querySelector(".static-page__back-to-top");
+
+  const scrollContainer = () => {
+    return document.documentElement || document.body;
+  };
+
+  document.addEventListener("scroll", () => {
+    if (scrollContainer().scrollTop > showOnPx) {
+      backToTopButton.style.opacity = "100%";
     } else {
-      answer.style.display = "block";
+      backToTopButton.style.opacity = "0%";
     }
   });
-}
 
-const showOnPx = 100;
-const backToTopButton = document.querySelector(".static-page__back-to-top");
+  const goToTop = () => {
+    document.body.scrollIntoView({
+      behavior: "smooth",
+    });
+  };
 
-const scrollContainer = () => {
-  return document.documentElement || document.body;
-};
+  backToTopButton.addEventListener("click", goToTop);
 
-document.addEventListener("scroll", () => {
-  if (scrollContainer().scrollTop > showOnPx) {
-    backToTopButton.style.opacity = "100%";
-  } else {
-    backToTopButton.style.opacity = "0%";
+  const links = document.querySelectorAll(".static-page__list-link");
+
+  for (const link of links) {
+    link.addEventListener("click", clickHandler);
   }
-});
 
-const goToTop = () => {
-  document.body.scrollIntoView({
-    behavior: "smooth",
-  });
-};
+  function clickHandler(e) {
+    const href = this.getAttribute("href");
+    if (!href.startsWith("#")) {
+      /* bubble event up, ie. open external links */
+      return;
+    }
+    e.preventDefault();
+    history.pushState(undefined, "", href);
+    const offsetTop = document.querySelector(href).offsetTop;
 
-backToTopButton.addEventListener("click", goToTop);
-
-const links = document.querySelectorAll(".static-page__list-link");
-
-for (const link of links) {
-  link.addEventListener("click", clickHandler);
+    scroll({
+      top: offsetTop,
+      behavior: "smooth",
+    });
+  }
 }
-
-function clickHandler(e) {
-  e.preventDefault();
-  const href = this.getAttribute("href");
-  const offsetTop = document.querySelector(href).offsetTop;
-
-  scroll({
-    top: offsetTop,
-    behavior: "smooth",
-  });
+if (!window.fragorHandlersInitialized) {
+  init();
 }
