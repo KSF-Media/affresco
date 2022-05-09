b786c77b7b476e00808dd89711f1b7086e7e7283
MicciKarkia <60072001+MicciKarkia@users.noreply.github.com>
2022-05-09 15:03:15 +0300

    Mosaico/static pages for vn and on (#1057)
    * Add function to read /static recursively
    
    * Remove html files from static folder
    
    * Copy html files to hbl, vn and on directories
    
    * Copy recursively
    
    * Fetch static page html file based on paper
    
    * Remove log function
    
    * Add folder name to path
    
    Co-authored-by: Valtteri Pajunen <valtteri.pajunen@ksfmedia.fi>

--- apps/mosaico/src/Mosaico/StaticPage.purs
+++ apps/mosaico/src/Mosaico/StaticPage.purs
@@ -11,6 +11,7 @@ import Data.Monoid (guard)
 import Data.Nullable (Nullable)
 import Effect (Effect)
 import Effect.Aff (Aff)
+import Mosaico.Paper (_mosaicoPaper)
 
 foreign import getInitialStaticPageContent :: Effect (Nullable String)
 foreign import getInitialStaticPageScript :: Effect (Nullable String)
@@ -28,7 +29,9 @@ type StaticPage =
 
 fetchStaticPage :: String -> Aff StaticPageResponse
 fetchStaticPage pageName = do
-  let staticPageUrl = "/assets/" <> pageName <> ".html"
+  --Static pages' html files can have different content depending on the paper, therefore they are found in paper specific directory.
+  --The js files have the same content regardless of paper, therefore they are found directly in the static directory
+  let staticPageUrl = "/assets/" <> _mosaicoPaper <> "/" <> pageName <> ".html"
   let staticPageJsUrl = "/assets/" <> pageName <> ".js"
   resPage <- AX.get AX.string staticPageUrl
   resJs <- AX.get AX.string staticPageJsUrl
