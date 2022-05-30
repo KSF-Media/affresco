3cc33d0c0fe5736aa9e7400ed48d7351ba75cae0
MicciKarkia <60072001+MicciKarkia@users.noreply.github.com>
2022-05-30 15:41:22 +0300

    Mosaico/style epaper page (#1170)
    * Change table image to new one
    
    * Style e-paper for all screens
    
    * Add tablet image to HBL Junior

--- apps/mosaico/src/Mosaico/Epaper.purs
+++ apps/mosaico/src/Mosaico/Epaper.purs
@@ -59,7 +59,7 @@ render onLogin paper loadingUser userAuth entitlements =
                 [ DOM.a
                     { className: "mosaico-epaper--teaser"
                     , href: if entitled then latestEpaper paper else "https://prenumerera.ksfmedia.fi/#/" <> Paper.cssName paper
-                    , children: [ DOM.img { src: "https://cdn.ksfmedia.fi/mosaico/tablet.png" } ]
+                    , children: [ DOM.img { src: "https://cdn.ksfmedia.fi/mosaico/tablet-bg.png" } ]
                     }
                 , DOM.div
                     { className: "mosaico-epaper--body"
@@ -86,6 +86,7 @@ render onLogin paper loadingUser userAuth entitlements =
         , children:
             [ DOM.div
                 { className: "mosaico-epaper--teaser mosaico-epaper--teaser__junior"
+                , children: [ DOM.img { src: "https://cdn.ksfmedia.fi/mosaico/tablet-bg.png" } ]
                 }
             , DOM.div
                 { className: "mosaico-epaper--body"
