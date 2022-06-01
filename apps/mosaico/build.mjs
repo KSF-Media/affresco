006084cc4751c8a85e1516f964be429940b1144d
Jaakko Hannikainen <jaakko.hannikainen@bytecraft.fi>
2022-06-01 14:33:28 +0300

    Manually fix HTML src paths

--- apps/mosaico/build.mjs
+++ apps/mosaico/build.mjs
@@ -1,22 +1,71 @@
 import esbuild from "esbuild";
 import { buildSettings, transpileIfMinify } from "../../esbuild.defaults.mjs";
-import htmlPlugin from "@chialab/esbuild-plugin-html";
-import { lessLoader } from "esbuild-plugin-less";
 import * as util from "util";
 import glob from "tiny-glob";
+import * as fs from "fs";
 import child_process from "child_process";
+import * as cheerio from "cheerio";
+import { create } from "browser-sync";
+
 const exec = util.promisify(child_process.exec);
+const writeFile = util.promisify(fs.writeFile);
+const rmFile = util.promisify(fs.rm);
 
 const mainSettings = {
   ...buildSettings,
-  assetNames: "assets/[name]-[hash]",
-  chunkNames: "assets/[name]-[hash]",
-  outdir: "dist",
+  assetNames: "[name]-[hash]",
+  chunkNames: "[name]-[hash]",
+  outdir: "dist/assets",
   entryPoints: ["./web/index.html"],
 };
 
-export function buildMain() {
-  return esbuild.build(mainSettings).then(transpileIfMinify(mainSettings));
+function rewriteIndexHtmlPathsToAbsolute(result) {
+  // The HTML builder doesn't use publicPath for some reason...
+  // so we have to fix the src=index.hash.js link to src=/index.hash.js
+  let outfiles = Object.keys(result.metafile.outputs);
+
+  const templateFile = "./dist/assets/index.html";
+  const template = cheerio.load(fs.readFileSync(templateFile, "utf8"));
+
+  template("script, link[rel=stylesheet]").each((ix, elem) => {
+    const src = template(elem).attr("src");
+    const href = template(elem).attr("href");
+
+    const assetName = src || href;
+    if (!assetName || assetName.indexOf("//") >= 0) {
+      return;
+    }
+
+    const publicUrl = process.env.PUBLIC_URL || "";
+    if (src) {
+      template(elem).attr("src", publicUrl + "/assets/" + assetName);
+      //.attr("class", undefined);
+    } else if (href) {
+      template(elem).attr("href", publicUrl + "/assets/" + assetName);
+      //.attr("class", undefined);
+    }
+  });
+
+  return writeFile("./dist/index.html", template.html()).then(() =>
+    rmFile("./dist/assets/index.html", { force: true })
+  );
+}
+
+export function buildMain(watch) {
+  return esbuild
+    .build({
+      ...mainSettings,
+      watch: watch
+        ? {
+            onRebuild: (error, result) => {
+              if (error) console.error("watch build failed:", error);
+              else return rewriteIndexHtmlPathsToAbsolute(result);
+            },
+          }
+        : watch,
+    })
+    .then(transpileIfMinify(mainSettings))
+    .then(rewriteIndexHtmlPathsToAbsolute);
 }
 
 export async function buildStatic(watch) {
@@ -45,22 +94,48 @@ export async function buildStatic(watch) {
 }
 
 function runBuild() {
-  return buildMain().then(() => buildStatic(false));
+  return buildMain(false).then(() => buildStatic(false));
 }
 
 async function buildOrServe() {
   if (process.argv.some((arg) => arg == "serve")) {
-    const port = parseInt(process.env.PORT, 10) || 3000;
-    await buildStatic(false);
-    buildStatic(true);
-    console.log("Running in http://localhost:" + port);
-    return esbuild.serve(
-      {
-        servedir: mainSettings.outdir,
-        port,
-      },
-      mainSettings
-    );
+    const statics = buildStatic(true);
+    const main = buildMain(true);
+
+    return Promise.all([statics, main]).then(() => {
+      const bs = create();
+      bs.watch("dist/**/*").on("change", (file) => bs.reload(file));
+
+      function redirToRoot(req, res, next) {
+        res.writeHead(302, {
+          Location: "/",
+        });
+        req.url = "/";
+        res.end();
+        return next();
+      }
+
+      return bs.init({
+        server: {
+          baseDir: "dist",
+          index: "index.html",
+        },
+        serveStatic: [
+          {
+            route: "/assets",
+            dir: "./dist/assets",
+          },
+        ],
+        middleware: [
+          {
+            route: "/artikel",
+            handle: redirToRoot,
+          },
+          { route: "/sida", handle: redirToRoot },
+        ],
+        single: true,
+      });
+    });
   }
   return runBuild();
 }
