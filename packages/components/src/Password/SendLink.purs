fce80146e4068fd1dcffb375b6ee9b4a07fdd5c8
MicciKarkia <60072001+MicciKarkia@users.noreply.github.com>
2022-07-04 12:35:21 +0300

    Mitt konto/fix password reset styling (#1259)
    * Add font size, change placeholder, fix row-gap
    
    * Fix styling & spelling, move info for form to form
    
    Co-authored-by: Valtteri Pajunen <valtteri.pajunen@ksfmedia.fi>

--- packages/components/src/Password/SendLink.purs
+++ packages/components/src/Password/SendLink.purs
@@ -63,13 +63,15 @@ requestResetLink = do
             , id: "forgot-password-form"
             , onSubmit: Events.handler preventDefault $ const $ submit validatedForm
             , children:
-                [ DOM.div
+                [ DOM.div_ [ DOM.text "Ange den e-postadress du skapade kontot med." ]
+                , DOM.div_ [ DOM.text "Ifall mejlet inte kommer fram kan du behöva kolla din skräppost." ]
+                , DOM.div
                     { className: "password--reset-field"
                     , children:
                         [ InputField.inputField
                             { type_: InputField.Email
                             , name: "email"
-                            , placeholder: "E-post"
+                            , placeholder: "Fyll i din e-postadress"
                             , value: email
                             , onChange: setEmail
                             , label: Just "E-post"
@@ -89,17 +91,19 @@ requestResetLink = do
         renderSuccess =
           DOM.div
             { className: "success-text"
-            , children: [ DOM.text "Vi har skickat ett e-post med instruktioner för att skapa ett nytt lösenord." ]
+            , children: [ DOM.h2_ [ DOM.text "Tack!"]
+                        , DOM.text "Om e-postadressen finns i vårt system har vi skickat ett mejl med instruktioner för att skapa ett nytt lösenord."
+                        ]
             }
 
         renderFailure =
           DOM.div
             { className: "error-text"
-            , children: [ DOM.text "Något gick fel."
+            , children: [ DOM.text "Oj, något gick fel."
                         , DOM.a
                             { href: "/#lösenord"
                             , onClick: capture_ resetForm
-                            , children: [ DOM.text "Försök igen." ]
+                            , children: [ DOM.text "Vänligen försök igen." ]
                             }
                         ]
             }
