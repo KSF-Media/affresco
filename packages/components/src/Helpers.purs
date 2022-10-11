522aca9f6cbac6eb71709961d8a3bf07f3df7f73
Kari Pahula <kaol@iki.fi>
2022-10-11 17:22:27 +0300

    Apply noon rule for subscription pauses as well

--- packages/components/src/Helpers.purs
+++ packages/components/src/Helpers.purs
@@ -2,19 +2,22 @@ module KSF.Helpers where
 
 import           Prelude
 
-import           Data.Date               (Date)
+import           Control.Alt ((<|>))
+import           Data.Date               (Date, adjust)
 import           Data.DateTime           (DateTime (..))
+import           Data.DateTime           as DateTime
 import           Data.Enum               (toEnum)
 import           Data.Formatter.DateTime (Formatter, FormatterCommand (..),
                                           format)
 import           Data.Int                (toNumber)
 import           Data.List               (fromFoldable)
-import           Data.Maybe              (fromJust)
+import           Data.Maybe              (Maybe (..), fromJust)
 import           Data.String             (Pattern (..))
 import           Data.String             as String
 import           Data.Time               (Time (..))
-import           Data.Time.Duration      (Hours (..))
+import           Data.Time.Duration      (Days (..), Hours (..), negateDuration)
 import           Effect                  (Effect)
+import           Effect.Now              as Now
 import           Partial.Unsafe          (unsafePartial)
 
 foreign import getCurrentTZOffset_ :: Effect Int
@@ -95,3 +98,18 @@ formatEur amountCent =
 
 paperInvoiceCents :: Int
 paperInvoiceCents = 500
+
+-- If doing temporary address change or pause before 12:00, allow it
+-- to start from next day.  Otherwise, 2 days from now.
+getMinStartDate :: Maybe Date -> Effect (Maybe Date)
+getMinStartDate nextDelivery = do
+  offset <- negateDuration <$> getCurrentTZOffset
+  localNow <- DateTime.adjust offset <$> Now.nowDateTime
+  case localNow of
+    -- Shouldn't happen
+    Nothing -> pure Nothing
+    Just now -> do
+      let soonestDuration = Days $ if DateTime.time now < noon then 1.0 else 2.0
+          soonestStart = adjust soonestDuration (DateTime.date now)
+          byNextIssue = max <$> soonestStart <*> nextDelivery
+      pure $ byNextIssue <|> soonestStart
