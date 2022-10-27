801c648cbbf934bbe59b0592329983b9df88dfb1
Jaakko Hannikainen <91122811+jaakkoha-bytecraft@users.noreply.github.com>
2022-10-27 11:46:42 +0300

    Fix janrain login error callback not being called (#1324)

--- packages/user/src/User/Login/JanrainSSO.js
+++ packages/user/src/User/Login/JanrainSSO.js
@@ -12,5 +12,17 @@ export function loadConfig() {
     xd_receiver: baseUrl + process.env.JANRAIN_XD_RECEIVER_PATH,
   };
   console.log("SSO config", config);
+  /* In order for the failure callback to work, the config object needs to have
+   * a field called nologin_callback with the value "nologin_callback", but for
+   * some reason (bug in Janrain's SSO code) we can't set it through the object
+   * we are passing in. Luckily the object is passed to the function below, so
+   * we can patch the function... */
+  if (window.JANRAIN && window.JANRAIN.SSO && window.JANRAIN.SSO.check_login_dispatch) {
+    var oldfn = window.JANRAIN.SSO.check_login_dispatch;
+    window.JANRAIN.SSO.check_login_dispatch = (a, b) => {
+      a.nologin_callback = "nologin_callback";
+      oldfn(a, b);
+    };
+  }
   return window.JANRAIN ? config : null;
-};
+}
