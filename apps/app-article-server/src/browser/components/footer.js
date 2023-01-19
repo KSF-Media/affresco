6a5e918d300d3a70f7529a71c39c0c402614474b
Staffan Småros <staffan.smaros@ksfmedia.fi>
2023-01-19 12:18:12 +0200

    Changed footer response link to contact

--- apps/app-article-server/src/browser/components/footer.js
+++ apps/app-article-server/src/browser/components/footer.js
@@ -6,12 +6,23 @@ const getBrandMessage = (brand) => {
   }
 };
 
+const getSiteDomainName = (brand) => {
+  switch (brand) {
+    case "vn":
+      return "vastranyland"
+    case "on":
+      return "ostnyland"
+    default:
+      return "hbl"
+  }
+}
+
 const Footer = (props) => {
   return (
     <div className={"footer"}>
       <h4 className={"headline"}>{getBrandMessage(props.brandValueName)}</h4>
-      <a href="https://docs.google.com/forms/d/e/1FAIpQLSdORLcRq_URIS7deTkgOKfGd1fy59lkdTEtEbt5bzvaADCDKw/viewform?usp=sf_link">
-        Ge respons
+      <a href={`https://www.${getSiteDomainName(props.brandValueName)}.fi/sida/kontakt`}>
+        Ta kontakt
       </a>
       <br />
       <strong>KSF Media {new Date().getFullYear()}</strong>
