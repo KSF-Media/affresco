5e7c7b29b56e9e780e1aa7a6dfae26d964652b83
Max Fahler <max.fahler@ksfmedia.fi>
2023-04-12 16:10:11 +0300

    fix formatting

--- apps/prenumerera/src/Analytics/Analytics.js
+++ apps/prenumerera/src/Analytics/Analytics.js
@@ -1,23 +1,23 @@
 if (typeof window !== "undefined") {
-    window.dataLayer = window.dataLayer || [];
-  }
-  
-  export function _purchase(cusno, packageId, method, price) {
-    const date = new Date()
-    dataLayer.push({
-      event: "purchase",
-      ecommerce: {
-        currency: "EUR",
-        transaction_id: cusno + "." + packageId + "." + date.toISOString(),
-        payment_type: method,
-        value: price,
-        items: [{
+  window.dataLayer = window.dataLayer || [];
+}
+
+export function _purchase(cusno, packageId, method, price) {
+  const date = new Date();
+  dataLayer.push({
+    event: "purchase",
+    ecommerce: {
+      currency: "EUR",
+      transaction_id: cusno + "." + packageId + "." + date.toISOString(),
+      payment_type: method,
+      value: price,
+      items: [
+        {
           item_name: packageId,
           price: price,
-          quantity: 1
-        }]
-      }
-    }
-    )
-  }
-  
+          quantity: 1,
+        },
+      ],
+    },
+  });
+}
