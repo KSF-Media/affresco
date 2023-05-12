33a4a73371e568a77ef314b61b5eed7f4019b5d1
Jaakko Hannikainen <jaakko.hannikainen@bytecraft.fi>
2023-05-12 15:17:15 +0300

    npx prettier --write apps/hbl365

--- apps/hbl365/README.md
+++ apps/hbl365/README.md
@@ -1,4 +1,5 @@
 # HBL365 purchase path
+
 This site contains the purchase path for the HBL365 app (https://365.ksfmedia.fi/)
 
 ## Running locally
@@ -8,15 +9,17 @@ yarn install
 yarn build
 yarn start
 ```
+
 A local dev server can now be found @ http://localhost:8004/
 
 ## Running tests
+
 ```bash
 yarn test
 ```
 
 ## Deployment
 
-When a PR is merged to __master__ it gets __deployed__ to __production__
+When a PR is merged to **master** it gets **deployed** to **production**
 
 Deployment is handled by github actions, more details in the CI [README](../../ci/README.md)
--- apps/hbl365/src/Main.js
+++ apps/hbl365/src/Main.js
@@ -1,18 +1,18 @@
-import apple from '../../../../images/hbl365/apple.png';
-import android from '../../../../images/hbl365/android.png';
-import logo from '../../../../images/hbl365/app-icon-1024x1024-pixels.png';
+import apple from "../../../../images/hbl365/apple.png";
+import android from "../../../../images/hbl365/android.png";
+import logo from "../../../../images/hbl365/app-icon-1024x1024-pixels.png";
 
 export const appStore = { apple, android };
 
 export { logo };
 
 export function addOnScroll() {
-    const header = document.getElementsByTagName('header')[0];
-    window.onscroll = function(ev) {
-	if (window.scrollY > 38) {
-	    header.classList.add('tight');
-	} else {
-	    header.classList.remove('tight');
-	}
+  const header = document.getElementsByTagName("header")[0];
+  window.onscroll = function (ev) {
+    if (window.scrollY > 38) {
+      header.classList.add("tight");
+    } else {
+      header.classList.remove("tight");
     }
+  };
 }
