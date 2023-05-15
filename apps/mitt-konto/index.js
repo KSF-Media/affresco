37fab74bcd7fda00582ffb65cc1815a105af99d8
Jaakko Hannikainen <jaakko.hannikainen@bytecraft.fi>
2023-05-15 10:32:02 +0300

    mitt-konto: fix lints

--- apps/mitt-konto/index.js
+++ apps/mitt-konto/index.js
@@ -2,9 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "basscss/css/basscss-cp.css";
 
-// yup, welcome to react 16
-import createReactClass from "create-react-class";
-React.createClass = createReactClass;
 var Main = require("./output/MittKonto.Main/index.js");
 
 function main() {
