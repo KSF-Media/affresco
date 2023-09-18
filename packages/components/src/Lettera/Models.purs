99ead271550b05ac285b35b3dfe74c2e3724f842
John Lång <john.lang@platonic.systems>
2023-09-18 13:46:29 +0300

    Move lettera error type to Lettera models module and implement EncodeJson for it

--- packages/components/src/Lettera/Models.purs
+++ packages/components/src/Lettera/Models.purs
@@ -2,15 +2,20 @@ module Lettera.Models where
 
 import Prelude
 
+import Affjax (Error, Response, Request, printError) as AX
+import Affjax.RequestHeader (RequestHeader(..)) as AX
+import Affjax.ResponseHeader (ResponseHeader(..)) as AX
+import Affjax.StatusCode (StatusCode(..)) as AX
 import Data.Argonaut.Core (Json, caseJsonObject, jsonEmptyObject)
 import Data.Argonaut.Decode (class DecodeJson, JsonDecodeError(..), decodeJson, getField, printJsonDecodeError, (.!=), (.:), (.:?))
 import Data.Argonaut.Decode.Decoders (decodeJArray, decodeJObject, decodeString)
-import Data.Argonaut.Encode (class EncodeJson, encodeJson, (:=), (~>))
+import Data.Argonaut.Encode (class EncodeJson, extend, (:=), (:=?), (~>), (~>?))
+import Data.Argonaut.Encode.Class (encodeJson)
 import Data.Argonaut.Encode.Encoders (encodeInt, encodeString)
 import Data.Array (catMaybes, mapMaybe)
 import Data.DateTime (DateTime, adjust)
 import Data.Either (Either(..), hush)
-import Data.Foldable (foldMap, foldr, lookup)
+import Data.Foldable (class Foldable, foldMap, foldl, foldr, lookup)
 import Data.Formatter.DateTime (format, unformat)
 import Data.Generic.Rep (class Generic)
 import Data.Hashable (class Hashable, hash)
@@ -19,6 +24,7 @@ import Data.JSDate as JSDate
 import Data.Map (Map)
 import Data.Map as Map
 import Data.Maybe (Maybe(..), fromMaybe, maybe, fromJust)
+import Data.MediaType (MediaType(..))
 import Data.Newtype (class Newtype, un, unwrap)
 import Data.Set (Set, fromFoldable) as Set
 import Data.Show.Generic (genericShow)
@@ -750,3 +756,106 @@ editorialIdToUuid editorialId =
 --   - https://datatracker.ietf.org/doc/html/rfc4122
 url_namespace :: UUID.UUID
 url_namespace = unsafePartial $ fromJust $ UUID.parseUUID "6ba7b811-9dad-11d1-80b4-00c04fd430c8"
+
+encodeRequestHeaders :: Array AX.RequestHeader -> Maybe Json
+encodeRequestHeaders [] = Nothing
+encodeRequestHeaders hs =
+  let encodeHeader h obj = flip extend obj $ case h of
+        AX.Accept (MediaType v)      -> "Accept" := v
+        AX.ContentType (MediaType v) -> "ContentType" := v
+        AX.RequestHeader k v         -> k := v
+  in Just $ foldr encodeHeader jsonEmptyObject hs
+
+encodeRequest :: forall a . AX.Request a -> Json
+encodeRequest x =
+  let method = case x.method of
+        Left m  -> encodeJson (show m)
+        Right s -> encodeJson (show s)
+  in "method"  := method ~>
+     "url"     := x.url  ~>
+     "headers" :=? encodeRequestHeaders x.headers ~>?
+     -- Currently we're only using GET requests so we don't need content:
+     -- "content" := x.content ~>
+     jsonEmptyObject
+
+encodeResponseHeaders :: Array AX.ResponseHeader -> Json
+encodeResponseHeaders hs =
+  let encodeHeader (AX.ResponseHeader k v) obj = k := v ~> obj
+  in foldr encodeHeader jsonEmptyObject hs
+
+encodeResponse :: forall a . AX.Response a -> Json
+encodeResponse x =
+  let statusCode = case x.status of
+        AX.StatusCode n -> n
+  in "statusCode" := statusCode ~>
+--     "statusText" := x.statusText ~>
+     "headers" := encodeResponseHeaders x.headers ~>
+     jsonEmptyObject
+
+encodeError :: String -> Maybe Json -> Maybe Json -> Maybe Json -> Json
+encodeError name req err code =
+  "err" := name ~>
+  (("req" := _) <$> req) ~>?
+  (("msg" := _) <$> err) ~>?
+  (("code" := _) <$> code) ~>?
+  jsonEmptyObject
+
+instance encodeJsonLetteraError :: EncodeJson LetteraError where
+  encodeJson e = case e of
+    ResponseError { letteraErrorRequest, letteraErrorError } ->
+      encodeError "ResponseError"
+                  (Just letteraErrorRequest)
+                  (Just (encodeString (AX.printError letteraErrorError)))
+                  Nothing
+    HttpError { letteraErrorRequest, letteraErrorResponse, letteraErrorCode } ->
+      encodeError "HttpError"
+                  (Just letteraErrorRequest)
+                  (Just letteraErrorResponse)
+                  (Just (encodeInt letteraErrorCode))
+    ParseError ->
+      encodeError "ParseErrror" Nothing Nothing Nothing
+
+data LetteraError
+  = ResponseError
+    { letteraErrorRequest  :: Json
+    , letteraErrorError    :: AX.Error
+    }
+  | HttpError
+    { letteraErrorRequest  :: Json
+    , letteraErrorResponse :: Json
+    , letteraErrorCode     :: Int
+    }
+  | ParseError -- TODO: Include some info for better error messages
+
+mkResponseError :: forall a. AX.Request a -> AX.Error -> LetteraError
+mkResponseError req err = ResponseError
+  { letteraErrorRequest: encodeRequest req
+  , letteraErrorError: err
+  }
+
+mkHttpError :: forall a b. AX.Request a -> AX.Response b -> Int -> LetteraError
+mkHttpError req resp code = HttpError
+  { letteraErrorRequest: encodeRequest req
+  , letteraErrorResponse: encodeResponse resp
+  , letteraErrorCode: code
+  }
+
+mkParseError :: LetteraError
+mkParseError = ParseError
+
+data LetteraResponse a = LetteraResponse
+  { maxAge :: Maybe Int
+  , body :: Either LetteraError a
+  }
+
+responseBody :: forall a. LetteraResponse a -> Maybe a
+responseBody (LetteraResponse a) = hush a.body
+
+instance functorLetteraResponse :: Functor LetteraResponse where
+  map f (LetteraResponse r@{ body }) =
+    LetteraResponse $ r { body = f <$> body }
+
+instance foldableLetteraResponse :: Foldable LetteraResponse where
+  foldl f z = foldl f z <<< responseBody
+  foldr f z = foldr f z <<< responseBody
+  foldMap f = foldMap f <<< responseBody
