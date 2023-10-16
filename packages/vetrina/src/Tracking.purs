f7671983e43d33d53c5187feac9176caab3bb903
Max Fahler <maxfahler@gmail.com>
2023-10-16 15:40:27 +0300

    changes to tracking

--- packages/vetrina/src/Tracking.purs
+++ packages/vetrina/src/Tracking.purs
@@ -5,9 +5,10 @@ import Data.Maybe (Maybe(..))
 import Effect (Effect)
 import Data.Function.Uncurried (Fn4, runFn4)
 import KSF.User (OrderNumber)
-import Prelude (Unit, pure, unit)
+import Prelude (Unit)
 
 foreign import transaction_ :: Fn4 OrderNumber String String String (Effect Unit)
+foreign import orderStarted_ :: Effect Unit
 
 transaction
   :: OrderNumber
@@ -17,4 +18,7 @@ transaction
   -> Effect Unit
 transaction orderNumber (Just productId) (Just productPrice) maybeCampaignNo =
   runFn4 transaction_ orderNumber productId productPrice (fold maybeCampaignNo)
-transaction _ _ _ _ = pure unit
+transaction orderNumber _ _ _ = runFn4 transaction_ orderNumber "" "" ""
+
+orderStarted :: Effect Unit
+orderStarted = orderStarted_
